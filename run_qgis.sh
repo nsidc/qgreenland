1ace718d91d8c982fa658124e9fccdd7821b978e
Trey Stafford <trey.stafford@nsidc.org>
2020-01-15 16:05:17 -0700

    Init

---
+++ run_qgis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+xhost +
+
+docker run -it \
+       -e DISPLAY=$DISPLAY \
+       -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+       -v $HOME/.local/share/QGIS/QGIS3/:/root/.local/share/QGIS/QGIS3/:rw \
+       -v $PWD/qgis-data/:/root/qgis-data/:rw \
+       qgis/qgis:release-3_10 qgis
+
+xhost -
