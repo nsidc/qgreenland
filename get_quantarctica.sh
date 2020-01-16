c5d7524bb8c881c7a3e8c51776aa7416fb26577d
Trey Stafford <trey.stafford@nsidc.org>
2020-01-16 10:34:00 -0700

    Fetch quantarctica3 archive and extract it.

--- get_quantarctica.sh
+++ get_quantarctica.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# TODO write me!
+set -ex
+
+URL="data.pgc.umn.edu/gis/packages/quantarctica"
+FILE="Quantarctica3.zip"
+
+if [ ! -f "qgis-data/${FILE}" ]; then
+  wget --show-progress "${URL}/${FILE}" --output-document "qgis-data/${FILE}"
+fi
+
+unzip -o "qgis-data/${FILE}" -d qgis-data/
