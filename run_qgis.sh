bd320d08c3d967ac40b3af411cd2390ee7116c23
Matt Fisher <matt.fisher@nsidc.org>
2020-01-23 11:11:44 -0700

    Add luigi data location to qgis container
    TODO: Standardize a data location

--- run_qgis.sh
+++ run_qgis.sh
@@ -9,6 +9,7 @@ docker run -it \
        -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
        -v $HOME/.local/share/QGIS/QGIS3/:/root/.local/share/QGIS/QGIS3/:rw \
        -v $PWD/qgis-data/:/root/qgis-data/:rw \
+       -v $PWD/luigi/data:/root/data/:rw \
        qgis/qgis:release-3_10 qgis
 
 xhost -
