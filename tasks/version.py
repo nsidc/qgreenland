f2637e79095456e5b6b091e3e4ad4799c92f5d96
Trey Stafford <trey.stafford@nsidc.org>
2020-02-24 17:07:47 -0700

    Error handling for invoke bumpversion task

--- tasks/version.py
+++ tasks/version.py
@@ -1,6 +1,12 @@
 from invoke import task
 
+from qgreenland import __version__
+
 
 @task
 def bump(ctx, part):
+    if part == 'release' and 'dev' not in __version__:
+        raise RuntimeError("Cannot bump version with part 'release': "
+                           f'current version is already a release: {__version__}')
+
     ctx.run(f'bumpversion {part}')
