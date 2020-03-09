c11af381ef9c17f3cad1480d68fb4d6cfb30b6af
Matt Fisher <matt.fisher@nsidc.org>
2020-03-09 16:00:18 -0600

    Rename layer_name -> layer_id

--- qgreenland/util/luigi.py
+++ qgreenland/util/luigi.py
@@ -7,19 +7,18 @@ from qgreenland.util.misc import get_layer_config, get_layer_fs_path
 
 
 class LayerConfigMixin(luigi.Task):
-    """Allow tasks to receive layer_name as parameter and get the correct config.
+    """Allow tasks to receive layer_id as parameter and get the correct config.
 
     Used for all tasks that require a layer config. This way, we only have to
     pass a string instead of a whole config object as a parameter.
     """
 
-    # TODO: Change to layer_id
-    layer_name = luigi.Parameter()
+    layer_id = luigi.Parameter()
     task_type = None
 
     @property
     def layer_cfg(self):
-        return get_layer_config(self.layer_name)
+        return get_layer_config(self.layer_id)
 
     @property
     def short_name(self):
