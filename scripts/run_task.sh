64556dbfc7e3e629bc49ee9e5b10ee3e669f2a95
Matt Fisher <matt.fisher@nsidc.org>
2020-03-16 10:30:34 -0600

    Add note about number of workers

--- scripts/run_task.sh
+++ scripts/run_task.sh
@@ -1,1 +1,9 @@
-docker-compose exec luigi luigi --module qgreenland.tasks.main ZipQGreenland
+# TODO: --workers=$(nproc)  (or more?)
+# Currently hardcoded to 1 because increasing number of workers leads to failures:
+#
+#    File "/luigi/tasks/qgreenland/util/qgis.py", line 87, in get_map_layer
+#        raise RuntimeError(f"Layer located at '{layer_path}' does not exist.")
+#        RuntimeError: Layer located at '/luigi/data/luigi-wip/qgreenland/Geophysics/IceBridge BedMachine/bedmachine_bed/bedmachine_bed.tif' does not exist.
+
+docker-compose exec luigi luigi --workers=1 \
+  --module qgreenland.tasks.main ZipQGreenland
