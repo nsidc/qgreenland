12d1686fa7ffdf2819b7c27da43fd1b44295a62f
Matt Fisher <matt.fisher@nsidc.org>
2020-03-18 12:46:36 -0600

    WIP Hydrologic Sub-Basins

---
+++ qgreenland/tasks/layers/rarred_shapefile.py
@@ -0,0 +1,28 @@
+from qgreenland.tasks.common.fetch import FetchDataFiles
+from qgreenland.tasks.common.misc import Unrar
+from qgreenland.tasks.common.shapefile import (ReprojectShapefile,
+                                               SubsetShapefile)
+from qgreenland.util.luigi import LayerPipeline
+
+
+class RarredShapefile(LayerPipeline):
+    """Rename files to their final location."""
+
+    def requires(self):
+        fetch_data = FetchDataFiles(
+            source_cfg=self.cfg['source'],
+            output_name=self.cfg['id']
+        )  # ->
+        unrar = Unrar(
+            requires_task=fetch_data,
+            layer_id=self.layer_id
+        )  # ->
+        reproject_shapefile = ReprojectShapefile(
+            requires_task=unzip,
+            layer_id=self.layer_id
+        )  # ->
+        # TODO: We don't always need/want to subset, so parameterize this
+        return SubsetShapefile(
+            requires_task=reproject_shapefile,
+            layer_id=self.layer_id
+        )
