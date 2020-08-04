d0330081fdf4b83c82ba666c53e57cf24467ee49
Trey Stafford <trey.stafford@nsidc.org>
2020-08-04 17:38:58 -0600

    Add missing `gzipped_shapefile_parts.py`
    ooooooooooooooooops

---
+++ qgreenland/tasks/layers/gzipped_shapefile_parts.py
@@ -0,0 +1,26 @@
+from qgreenland.tasks.common.fetch import FetchDataFiles
+from qgreenland.tasks.common.misc import UngzipMany
+from qgreenland.tasks.common.shapefile import (ReprojectShapefile,
+                                               SubsetShapefile)
+from qgreenland.util.luigi import LayerPipeline
+
+
+class GzippedShapefileParts(LayerPipeline):
+
+    def requires(self):
+        fetch_data = FetchDataFiles(
+            source_cfg=self.cfg['source'],
+            output_name=self.cfg['id']
+        )  # ->
+        unzip = UngzipMany(
+            requires_task=fetch_data,
+            layer_id=self.layer_id
+        )  # ->
+        reproject_shapefile = ReprojectShapefile(
+            requires_task=unzip,
+            layer_id=self.layer_id
+        )  # ->
+        return SubsetShapefile(
+            requires_task=reproject_shapefile,
+            layer_id=self.layer_id
+        )
