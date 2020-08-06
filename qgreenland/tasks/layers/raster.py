20ae4a65bd53e30f7528b65fa3bfe61d1ae01a87
Trey Stafford <trey.stafford@nsidc.org>
2020-08-06 17:43:14 -0600

    Add missing files :bell:
    oops

---
+++ qgreenland/tasks/layers/raster.py
@@ -0,0 +1,19 @@
+from qgreenland.tasks.common.fetch import FetchDataFiles
+from qgreenland.tasks.common.raster import BuildRasterOverviews, WarpRaster
+from qgreenland.util.luigi import LayerPipeline
+
+
+class Raster(LayerPipeline):
+    def requires(self):
+        fetch_data = FetchDataFiles(
+            source_cfg=self.cfg['source'],
+            output_name=self.cfg['id']
+        )  # ->
+        warp_raster = WarpRaster(
+            requires_task=fetch_data,
+            layer_id=self.layer_id
+        )  # ->
+        return BuildRasterOverviews(
+            requires_task=warp_raster,
+            layer_id=self.layer_id
+        )
