0a4ce70275732f97452cbaf34796785bf7e7fb4c
Matt Fisher <XXXXXXXXX>
2020-10-29 10:15:03 -0600

    Disable pseudo-TTY allocation for run_task script

--- scripts/run_task.sh
+++ scripts/run_task.sh
@@ -6,5 +6,5 @@
 #        RuntimeError: Layer located at '/luigi/data/luigi-wip/qgreenland/Geophysics/IceBridge BedMachine/bedmachine_bed/bedmachine_bed.tif' does not exist.
 
 # NOTE: Workers must be set to 1 for python debug breakpoints to be usable
-docker-compose exec luigi luigi --workers=1 \
+docker-compose exec -T luigi luigi --workers=1 \
   --module qgreenland.tasks.main ZipQGreenland
