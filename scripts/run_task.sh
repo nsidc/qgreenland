91d8309b8b14d395fbe805bee911af2c8b97aa3d
Matt Fisher <XXXXXXXXX>
2020-10-29 15:38:53 -0600

    Programatically configure docker-compose TTY behavior

--- scripts/run_task.sh
+++ scripts/run_task.sh
@@ -5,6 +5,15 @@
 #        raise RuntimeError(f"Layer located at '{layer_path}' does not exist.")
 #        RuntimeError: Layer located at '/luigi/data/luigi-wip/qgreenland/Geophysics/IceBridge BedMachine/bedmachine_bed/bedmachine_bed.tif' does not exist.
 
+# If this script is called from Jenkins, docker-compose's default TTY behavior
+# will not work. In other situations, we will want the ability to attach to a
+# debugger prompt.
+if [ -t 1 ]; then
+    tty_arg="-T"
+else
+    tty_arg=""
+fi
+
 # NOTE: Workers must be set to 1 for python debug breakpoints to be usable
-docker-compose exec -T luigi luigi --workers=1 \
+docker-compose exec ${tty_arg} luigi luigi --workers=1 \
   --module qgreenland.tasks.main ZipQGreenland
