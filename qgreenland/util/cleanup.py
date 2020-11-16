85d908fbe3b2702540ca0730f0d4d890741a58d9
Matt Fisher <XXXXXXXXX>
2020-11-16 14:56:48 -0700

    Lint :bell:

--- qgreenland/util/cleanup.py
+++ qgreenland/util/cleanup.py
@@ -91,38 +91,34 @@ def print_and_run(cmd, *, dry_run):
 @click.command(context_settings={'help_option_names': ['-h', '--help']})
 @click.option('dry_run', '--dry-run', '-d',
               help="Print commands, but don't actually delete anything.",
-              is_flag=True
-)
+              is_flag=True)
 @click.option('delete_inputs_by_pattern', '--delete-inputs-by-pattern', '-i',
               help=(
                   'Bash glob/brace pattern used to delete input datasources by'
                   ' `<dataset_id>.<source_id>`'
-))
+              ))
 @click.option('delete_wips_by_pattern', '--delete-wips-by-pattern', '-w',
               help=(
                   'Pattern used to delete WIP layers by layer ID'
-))
+              ))
 @click.option('delete_all_input', '--delete-all-input', '-I',
               help=(
                   'Delete _ALL_ input-cached layers, ignoring LAYER_ID_PATTERN'
               ),
               type=BOOLEAN_CHOICE, callback=_validate_boolean_choice,
-              default='False', show_default=True
-)
+              default='False', show_default=True)
 @click.option('delete_all_wip', '--delete-all-wip', '-W',
               help=(
                   'Delete _ALL_ WIP layers, ignoring LAYER_ID_PATTERN'
               ),
               type=BOOLEAN_CHOICE, callback=_validate_boolean_choice,
-              default='False', show_default=True
-)
+              default='False', show_default=True)
 @click.option('delete_compiled', '--delete-compiled', '-C',
               help=(
                   'Delete compiled (but not zipped) QGreenland datapackage'
               ),
               type=BOOLEAN_CHOICE, callback=_validate_boolean_choice,
-              default='True', show_default=True
-)
+              default='True', show_default=True)
 # TODO: delete_all_wip_tmp: Deletes dirs like `transform-luigi-tmp-4765361527/`
 #       from wip
 # TODO: delete_all_dev_releases?
@@ -131,8 +127,7 @@ def print_and_run(cmd, *, dry_run):
                   'Delete all zipped QGreenland releases'
               ),
               type=BOOLEAN_CHOICE, callback=_validate_boolean_choice,
-              default='False', show_default=True
-)
+              default='False', show_default=True)
 # NOTE: Complexity check (C901) is disabled because this function is just a big
 #       set of switches by design!
 def cleanup_cli(**kwargs):  # noqa: C901
