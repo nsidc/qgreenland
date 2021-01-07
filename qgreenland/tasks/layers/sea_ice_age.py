4d1f34ee5da210e77f5bddc5a93f300de1838028
Trey Stafford <trey.stafford@nsidc.org>
2021-01-07 13:03:17 -0700

    Implement `Commands` task for ice age
    Workaround for needing a unique gdalwarp workflow. To be improved upon moving forward.

--- qgreenland/tasks/layers/sea_ice_age.py
+++ qgreenland/tasks/layers/sea_ice_age.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+from qgreenland.constants import ASSETS_DIR
+from qgreenland.tasks.common.command import Commands
 from qgreenland.tasks.common.fetch import FetchDataFiles
 from qgreenland.tasks.common.misc import ExtractNcDataset
 from qgreenland.tasks.common.raster import GdalEdit, WarpRaster
@@ -5,8 +9,20 @@ from qgreenland.util.luigi import LayerPipeline
 
 
 class SeaIceAge(LayerPipeline):
+    """Experimental layer task using `Commands` task.
+
+    Do not copy.
+
+    This is a temporary hack to resolve a non-standard set of `gdalwarp`
+    operations to prevent smearing of data along the 45°E line.
+    """
     def requires(self):
         source = self.cfg['source']
+
+        commands = [
+            ['gdalwarp', '-t_srs', 'EPSG:3413', '-tr', '12500', '12500', '{INPUT}', '{OUTDIR}/reprojected.tif'],
+            ['gdalwarp', '-cutline', self.cfg['boundary']['fp'], '-crop_to_cutline', '{OUTDIR}/reprojected.tif', '{OUTPUT}'],
+        ]
 
         fetch_data = FetchDataFiles(
             source_cfg=source,
@@ -20,7 +36,8 @@ class SeaIceAge(LayerPipeline):
             requires_task=extract_nc_dataset,
             layer_id=self.layer_id,
         )  # ->
-        return WarpRaster(
+        return Commands(
             requires_task=gdal_edited,
+            commands=commands,
             layer_id=self.layer_id
         )
