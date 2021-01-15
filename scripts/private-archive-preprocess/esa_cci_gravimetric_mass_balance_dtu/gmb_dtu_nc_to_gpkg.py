2850aea808ab983a2bb5aef86a98d8d943b6c099
Trey Stafford <trey.stafford@nsidc.org>
2021-01-15 13:05:47 -0700

    Add `t` attribute to GMB geopackages
    According to the netcdf metadata, `t` is the "Average time for time slice"

--- scripts/private-archive-preprocess/esa_cci_gravimetric_mass_balance_dtu/gmb_dtu_nc_to_gpkg.py
+++ scripts/private-archive-preprocess/esa_cci_gravimetric_mass_balance_dtu/gmb_dtu_nc_to_gpkg.py
@@ -8,6 +8,7 @@ from netCDF4 import Dataset
 DATASET_ARCHIVE_LOCATION = '/share/appdata/qgreenland-private-archive/esa_cci_gravimetric_mass_balance_dtu/'
 DATA_PATH = os.path.join(DATASET_ARCHIVE_LOCATION, 'greenland_gravimetric_mass_balance_rl06_dtuspace_v2_0-170820/CCI_GMB_GIS.nc')
 OUTPUT_GPKG_PATH = os.path.join(DATASET_ARCHIVE_LOCATION, 'QGREENLAND_GEOPACKAGES')
+DATE_FORMAT = '%Y_%m_%d_%H_%M_%S'
 
 os.makedirs(OUTPUT_GPKG_PATH, exist_ok=True)
 
@@ -24,34 +25,37 @@ times = ds.variables['t'][:]
 datas = ds.variables['GMB_trend'][:]
 epoch_start = dt.datetime(2003, 1, 1)
 
-def _pop_schema(lat, lon, data):
+
+def _schema_formatted_data(lat, lon, data, date_str):
     return {
         'geometry': {
             'type': 'Point',
             'coordinates': (lon, lat)
         },
         'properties': {
-            'GMB_trend': data
+            'GMB_trend': data,
+            't': date_str
         }
     }
 
 gpkg_schema = {
     'geometry': 'Point',
     'properties': {
-        'GMB_trend': 'float'
+        'GMB_trend': 'float',
+        't': 'str'
     }
 }
 
 crs = from_epsg(4326)
 
-# TODO: use geopackage. Combine into one instead of many layers?
 for time_idx, time in enumerate(times):
     date = epoch_start + dt.timedelta(days=float(time))
+    date_str = date.strftime(DATE_FORMAT)
     with fiona.open(
-            os.path.join(OUTPUT_GPKG_PATH, f'points_{date:%Y_%m_%d_%H_%M_%S}.gpkg'),
+            os.path.join(OUTPUT_GPKG_PATH, f'points_{date_str}.gpkg'),
             'w',
             crs=crs,
             driver='GPKG',
             schema=gpkg_schema) as f:
         for lat, lon, data in zip(lats, lons, datas[:, time_idx]):
-            f.write(_pop_schema(float(lat), float(lon), float(data)))
+            f.write(_schema_formatted_data(float(lat), float(lon), float(data), date_str))
