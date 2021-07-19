ef1c6f362650389e5470cb0e35d22ec4ae2aaf4c
Matt Fisher <matt.fisher@nsidc.org>
2021-07-19 17:31:05 -0600

    Support TTY in cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker-compose exec luigi ./tasks/qgreenland/qgreenland/util/cleanup.py "$@"
+# If this script is called from Jenkins, docker-compose's default TTY behavior
+# will not work. In other situations, we will want the ability to attach to a
+# debugger prompt.
+if [ -t 1 ]; then
+    tty_arg=""
+else
+    tty_arg="-T"
+fi
+
+docker-compose exec ${tty_arg} luigi ./tasks/qgreenland/qgreenland/util/cleanup.py "$@"
