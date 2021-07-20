04ff1a59c2cb8a972706e5031ecb8ad706c92f94
Matt Fisher <matt.fisher@nsidc.org>
2021-07-20 14:20:07 -0600

    Implement string interpolation on command arguments

--- qgreenland/runners/command.py
+++ qgreenland/runners/command.py
@@ -9,45 +9,32 @@ def interpolate_args(
     **kwargs,
 ) -> List[str]:
     """Replace slugs in `args` with keys and values in `kwargs`."""
-    # probably just .format()
-    # TODO: COMMIT.
-
-    breakpoint()
-    pass
+    return [arg.format(**kwargs)
+            for arg in args]
 
 
-def command_runner(step: Step, *, input_dir: str, output_dir: str):
-    """Run a shell command.
+def command_runner(
+    step: Step,
+    *,
+    input_dir: str,
+    output_dir: str,
+):
+    """Run a shell command in the "gdal" conda environment.
 
-    Interpolate the following special slugs:
-
-        {output_dir} - self.output()
-        {input_dir} - self.input()  # [.path?]
+    `kwargs` are string-interpolated for each of the command's arguments.
     """
-    breakpoint()
-
     # TODO: Some better data structure; this access is confusing.
     command_args = step[0]
 
+    breakpoint()
     interpolate_args(
         command_args,
         input_dir=input_dir,
         output_dir=output_dir,
     )
 
-    subprocess(command_args)
-
-
-# class CommandRunner(LayerTask):
-#     command_args = luigi.ListParameter()
-#    
-#     def output(self):
-#         return luigi.LocalTarget(
-#             Path(self.outdir) / 
-#             f'command-{self.command_args[0]}',
-#         )
-# 
-#     def run(self):
-#         # TODO: Replace {input}, {output} slugs in commands
-# 
-#         run_ogr_command(self.command_args)
+    # TODO: What's an "ogr" command? Any command will work, this just runs the
+    # command in our special "gdal" conda environment. Rename the "gdal"
+    # environment to the "command" environment? Rename the function to
+    # "run_conda_command"? ¯\_(ツ)_/¯
+    run_ogr_command(command_args)
