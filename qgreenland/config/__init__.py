bf969c75f96b684ed2aa1ee7bb8402fb6964e788
Trey Stafford <trey.stafford@nsidc.org>
2021-08-02 13:14:32 -0600

    WIP init implementation of project models

--- qgreenland/config/__init__.py
+++ qgreenland/config/__init__.py
@@ -4,8 +4,11 @@ from qgreenland.util.config import make_config
 CONFIG = make_config(config_dir=CONFIG_DIR, schema_dir=CONFIG_SCHEMA_DIR)
 
 from qgreenland.models.config.layer import ConfigLayer
+from qgreenland.models.config.project import ConfigProject
 
 layer = CONFIG['layers']['background']
 # breakpoint()
 layer_conf = ConfigLayer(**layer)
+project = CONFIG['project']
+project_conf = ConfigProject(**project)
 breakpoint()
--- qgreenland/models/config/project.py
+++ qgreenland/models/config/project.py
@@ -1,8 +1,20 @@
-class ConfigProject:
+from typing import Any, Dict, List, Tuple
+
+from pydantic import BaseModel
+
+
+class ConfigBoundariesInfo(BaseModel):
+    fp: str
+    # TODO: create class for features? (keys: type, id, propperties, geometry
+    features: List[Dict[Any, Any]]
+    bbox: Tuple[float, float, float, float]
+
+
+class ConfigProjectBoundaries(BaseModel):
+    background: ConfigBoundariesInfo
+    data: ConfigBoundariesInfo
+
+
+class ConfigProject(BaseModel):
     crs: str
-    boundaries: ConfigProjectBoundaries 
-
-
-class ConfigProjectBoundaries:
-    background: str
-    data: str
+    boundaries: ConfigProjectBoundaries
