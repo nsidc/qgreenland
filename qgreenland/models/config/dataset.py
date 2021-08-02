4c7ffe92e4808abb71dc4f629e82155f3a583fb9
Trey Stafford <trey.stafford@nsidc.org>
2021-08-02 17:46:36 -0600

    WIP working pydantic models for dataset config

--- qgreenland/models/config/dataset.py
+++ qgreenland/models/config/dataset.py
@@ -1,27 +1,47 @@
-# Require a _subtype_ of ConfigDatasetAsset
-VariableAsset = TypeVar('VariableAsset', bound=ConfigDatasetAsset)
+from typing import Any, Dict, List
+
+from pydantic import BaseModel, validator
 
 
-class ConfigDataset:
-    id: str
-    assets: List[VariableAsset]
-    metadata: ConfigDatasetMetadata
-
-
-class ConfigDatasetMetadata:
-    title: str
-    abstract: str
-    citation: ConfigDatasetCitation
-
-
-class ConfigDatasetCitation:
-    text: str
-    url: str
-
-
-class ConfigDatasetAsset:
+class ConfigDatasetAsset(BaseModel):
     id: str
 
 
 class ConfigDatasetHttpAsset(ConfigDatasetAsset):
     urls: List[str]
+
+
+class ConfigDatasetCitation(BaseModel):
+    text: str
+    url: str
+
+
+class ConfigDatasetMetadata(BaseModel):
+    title: str
+    abstract: str
+    citation: ConfigDatasetCitation
+
+
+class ConfigDataset(BaseModel):
+    id: str
+    assets: List[Dict[str, Any]]
+    metadata: ConfigDatasetMetadata
+
+    # TODO: re-consider this approach...
+    @validator('assets')
+    def deref_assets(cls, value, values, **kwargs):
+        if not value:
+            # TOOD: better err and qgr exception
+            raise RuntimeError('PROBLEM!!!!')
+
+        assets = []
+        for asset in value:
+            if 'http' in asset.keys():
+                assets.append(ConfigDatasetHttpAsset(id=asset['id'], **asset['http']))
+            else:
+                # TODO: better err
+                raise RuntimeError('Unsupported asset configuration.')
+
+        return assets
+
+
