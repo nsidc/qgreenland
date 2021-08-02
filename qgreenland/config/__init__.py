0091df40fc33acbea4c2850780ad47240732cbd7
Trey Stafford <trey.stafford@nsidc.org>
2021-08-02 15:57:15 -0600

    WIP pydantic model for hierarchy settings

--- qgreenland/config/__init__.py
+++ qgreenland/config/__init__.py
@@ -5,9 +5,12 @@ CONFIG = make_config(config_dir=CONFIG_DIR, schema_dir=CONFIG_SCHEMA_DIR)
 
 from qgreenland.models.config.layer import ConfigLayer
 from qgreenland.models.config.project import create_project_config_model
+from qgreenland.models.config.hierarchy import HierarchySettings
 
 layer = CONFIG['layers']['background']
 layer_conf = ConfigLayer(**layer)
 project = CONFIG['project']
 project_conf = create_project_config_model(project)
+hierarchy = CONFIG['hierarchy_settings']
+hierarchy_conf = [HierarchySettings(path=path, **settings) for path, settings in hierarchy.items()]
 breakpoint()
--- qgreenland/models/config/hierarchy.py
+++ qgreenland/models/config/hierarchy.py
@@ -1,4 +1,19 @@
-class ConfigHierarchy:
-    # TODO : How to represent unknown keys? This is a mapping between paths and
-    # settings.
-    ...
+from typing import Dict
+
+from pydantic import BaseModel
+
+
+# TODO: maybe hierarchy config should just be a list of Hierarchy settings?
+class HierarchySettings(BaseModel):
+    path: str
+    show: bool
+    expand: bool
+
+
+# class ConfigHierarchy(BaseModel):
+#     # TODO : How to represent unknown keys? This is a mapping between paths and
+#     # settings. Maybe it makes most sense to leave this as a dict of HierarchySettings?
+#     __root__: Dict[str, HierarchySettings]
+#
+#     def __getitem__(self, item):
+#         return self.__root__[item]
