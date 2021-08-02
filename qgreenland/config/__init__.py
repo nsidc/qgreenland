09aa7527d95c1959b24c4047306a8bc99354ad8c
Trey Stafford <trey.stafford@nsidc.org>
2021-08-02 15:18:06 -0600

    WIP proof of concept for dynamic model generation (proj boundaries)

--- qgreenland/config/__init__.py
+++ qgreenland/config/__init__.py
@@ -4,11 +4,10 @@ from qgreenland.util.config import make_config
 CONFIG = make_config(config_dir=CONFIG_DIR, schema_dir=CONFIG_SCHEMA_DIR)
 
 from qgreenland.models.config.layer import ConfigLayer
-from qgreenland.models.config.project import ConfigProject
+from qgreenland.models.config.project import create_project_config_model
 
 layer = CONFIG['layers']['background']
-# breakpoint()
 layer_conf = ConfigLayer(**layer)
 project = CONFIG['project']
-project_conf = ConfigProject(**project)
+project_conf = create_project_config_model(project)
 breakpoint()
--- qgreenland/models/config/project.py
+++ qgreenland/models/config/project.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, List, Tuple
 
-from pydantic import BaseModel
+from pydantic import create_model, BaseModel
 
 
 class ConfigBoundariesInfo(BaseModel):
@@ -10,11 +10,21 @@ class ConfigBoundariesInfo(BaseModel):
     bbox: Tuple[float, float, float, float]
 
 
-class ConfigProjectBoundaries(BaseModel):
-    background: ConfigBoundariesInfo
-    data: ConfigBoundariesInfo
 
+# TODO: re-consider this...proof of concept for dynamic model generation.
+# In this case, it probably is OK to just statically define the project
+# boundaries ('data' and 'background'. Alternatively, in this case, perhaps it's
+# best just to define `ConfiProjectBoundaries` as Dict[str,
+# ConfigBoundariesInfo]. Processing would be fetching these data by key anyway?
+def create_project_config_model(project_config_dict):
 
-class ConfigProject(BaseModel):
-    crs: str
-    boundaries: ConfigProjectBoundaries
+    ConfigProjectBoundaries = create_model(
+        'ConfigProjectBoundaries',
+        **{k: (ConfigBoundariesInfo, ...) for k in project_config_dict['boundaries'].keys()}
+    )
+
+    class ConfigProject(BaseModel):
+        crs: str
+        boundaries: ConfigProjectBoundaries
+
+    return ConfigProject(**project_config_dict)
