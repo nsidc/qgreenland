cdbd13e6728a82e5880f2dfeb8234553c90a2343
Trey Stafford <trey.stafford@nsidc.org>
2021-08-03 17:31:18 -0600

    WIP use validator to dynamically select correct asset model

--- qgreenland/models/config/dataset.py
+++ qgreenland/models/config/dataset.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import List
 
 from pydantic import BaseModel, validator
 
@@ -23,7 +23,29 @@ class ConfigDatasetAsset(BaseModel):
         extra = 'allow'
 
 
+class ConfigDatasetHttpAsset(ConfigDatasetAsset):
+    # TODO: use pydantic URL type
+    urls: List[str]
+
+
 class ConfigDataset(BaseModel):
     id: str
     assets: List[ConfigDatasetAsset]
     metadata: ConfigDatasetMetadata
+
+    @validator('assets')
+    def validate_assets(cls, value, values, **kwargs):
+        # `value` is a list of assets.
+        if not value:
+            # TODO: better.
+            raise RuntimeError('Very problem.')
+
+        assets = []
+        for asset in value:
+            if 'http' in asset.dict():
+                assets.append(ConfigDatasetHttpAsset(id=asset.id, **asset.http))
+            else:
+                # TODO
+                raise RuntimeError('Only http assets are currently supported!!!')
+
+        return assets
