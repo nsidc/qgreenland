72f999a50318282638c36bfe7fba40bd411f4d73
Trey Stafford <trey.stafford@nsidc.org>
2021-08-02 18:04:38 -0600

    WIP init implementation of step template pydantic models

--- qgreenland/config/__init__.py
+++ qgreenland/config/__init__.py
@@ -7,6 +7,7 @@ from qgreenland.models.config.layer import ConfigLayer
 from qgreenland.models.config.project import create_project_config_model
 from qgreenland.models.config.hierarchy import HierarchySettings
 from qgreenland.models.config.dataset import ConfigDataset
+from qgreenland.models.config.step_template import ConfigStepTemplate
 
 layer = CONFIG['layers']['background']
 layer_conf = ConfigLayer(**layer)
@@ -16,4 +17,6 @@ hierarchy = CONFIG['hierarchy_settings']
 hierarchy_conf = [HierarchySettings(path=path, **settings) for path, settings in hierarchy.items()]
 datasets = CONFIG['datasets']
 dataset_conf = [ConfigDataset(**dataset) for dataset in datasets]
+step_templates = CONFIG['step_templates']  # Dict[str, Dict]
+step_templates_conf = [ConfigStepTemplate(name=name, **step_template) for name, step_template in step_templates.items()]
 breakpoint()
--- qgreenland/models/config/step_template.py
+++ qgreenland/models/config/step_template.py
@@ -1,3 +1,14 @@
-class ConfigStepTemplate:
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ConfigStep(BaseModel):
+    type: str
+    args: List[str]
+
+
+class ConfigStepTemplate(BaseModel):
+    name: str
     kwargs: List[str]
     steps: List[ConfigStep]
