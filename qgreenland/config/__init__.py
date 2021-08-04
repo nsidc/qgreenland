22941b1498719be0856f1c8b3dc07d465af664ac
Matt Fisher <matt.fisher@nsidc.org>
2021-08-04 17:06:44 -0600

    Add pydantic mypy plugin
    Cleanup unneeded config element

--- qgreenland/config/__init__.py
+++ qgreenland/config/__init__.py
@@ -2,9 +2,10 @@ from qgreenland.constants import CONFIG_DIR, CONFIG_SCHEMA_DIR
 from qgreenland.models.config import Config
 from qgreenland.models.config.dataset import ConfigDataset
 from qgreenland.models.config.hierarchy import HierarchySettings
-from qgreenland.models.config.step_template import ConfigStepTemplate
 from qgreenland.util.config import make_config
 
+
+# TODO: Consider doing dereferencing in pydantic validators
 init_conf = make_config(config_dir=CONFIG_DIR, schema_dir=CONFIG_SCHEMA_DIR)
 
 CONFIG = Config(
@@ -13,14 +14,6 @@ CONFIG = Config(
     datasets={
         dataset['id']: ConfigDataset(**dataset) for dataset in init_conf['datasets']
     },
-    # TODO: do we need step_templates in the config? Just useful at the
-    # dereferencing step when reading config from yaml?
-    step_templates=[
-        ConfigStepTemplate(
-            name=name,
-            **step_template
-        ) for name, step_template in init_conf['step_templates'].items()
-    ],
     hierarchy_settings=[
         HierarchySettings(
             path=path,
--- qgreenland/models/config/__init__.py
+++ qgreenland/models/config/__init__.py
@@ -6,12 +6,10 @@ from qgreenland.models.config.dataset import ConfigDataset
 from qgreenland.models.config.hierarchy import HierarchySettings
 from qgreenland.models.config.layer import ConfigLayer
 from qgreenland.models.config.project import ConfigProject
-from qgreenland.models.config.step_template import ConfigStepTemplate
 
 
 class Config(BaseModel):
     project: ConfigProject
     layers: Dict[str, ConfigLayer]
     datasets: Dict[str, ConfigDataset]
-    step_templates: List[ConfigStepTemplate]
     hierarchy_settings: List[HierarchySettings]
