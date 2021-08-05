864032349c84dcfddd2fc2428af588345dcc6f76
Trey Stafford <trey.stafford@nsidc.org>
2021-08-05 11:27:26 -0600

    Datasets pydantic model: Dynamic asset model selection with TypeVar

--- qgreenland/models/config/dataset.py
+++ qgreenland/models/config/dataset.py
@@ -1,7 +1,7 @@
 from abc import ABC
-from typing import Any, List
+from typing import Generic, List, Literal, TypeVar
 
-from pydantic import AnyUrl, BaseModel, Field, validator
+from pydantic import AnyUrl, BaseModel, Field
 
 
 class ConfigDatasetCitation(BaseModel):
@@ -20,38 +20,26 @@ class ConfigDatasetAsset(BaseModel, ABC):
 
 
 class ConfigDatasetHttpAsset(ConfigDatasetAsset):
+    type: Literal['http']
     urls: List[AnyUrl]
 
 
 class ConfigDatasetCmrAsset(ConfigDatasetAsset):
+    type: Literal['cmr']
     granule_ur: str = Field(..., min_length=1)
     collection_concept_id: str = Field(..., min_length=1)
 
 
+# It is unclear why we cannot just use `bound=ConfigDatasetAsset`, but suspect
+# that pydantic does not try to find all subclasses of `ConfigDatasetAsset` when
+# casting data to an appropriate model. Explicitly listing out the possible
+# types allows pydantic to select the correct model based on the input data.
+AnyAsset = TypeVar('AnyAsset', ConfigDatasetHttpAsset, ConfigDatasetCmrAsset)
+
 # ... ogr_remote_vector, manual assets
 
 
-class ConfigDataset(BaseModel):
+class ConfigDataset(BaseModel, Generic[AnyAsset]):
     id: str = Field(..., min_length=1)
-    assets: List[ConfigDatasetAsset] = Field(..., min_items=1)
+    assets: List[AnyAsset] = Field(..., min_items=1)
     metadata: ConfigDatasetMetadata
-
-    # TODO: Better type than List[Any]?
-    @classmethod
-    @validator('assets')
-    def handle_asset_subtypes(cls, value: List[Any]) -> List[Any]:
-        """."""
-        assets = []
-        mapping = {
-            'http': ConfigDatasetHttpAsset,
-        }
-        for asset in value:
-            if asset.type in mapping.keys():
-                assets.append(mapping[asset.type](**asset))
-            else:
-                raise RuntimeError(
-                    f'Only {mapping.keys()} assets are currently supported.'
-                )
-
-        # breakpoint()
-        return assets
