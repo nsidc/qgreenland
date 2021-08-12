334a0d1fe960a20a679e4959993fd81898a4b8cf
Matt Fisher <matt.fisher@nsidc.org>
2021-08-12 10:58:47 -0600

    Mock the layer dir instead of the full filepath

--- qgreenland/test/util/test_qgis.py
+++ qgreenland/test/util/test_qgis.py
@@ -1,5 +1,6 @@
 # import copy
 import os
+from enum import Enum
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
@@ -8,16 +9,14 @@ import qgis.core as qgc
 from qgreenland.constants import PACKAGE_DIR
 from qgreenland.util.qgis import layer as qgl
 
-mock_raster_path = Path(PACKAGE_DIR, 'test', 'data', 'example.tif')
+test_layers_dir = Path(PACKAGE_DIR) / 'test' / 'data' / 'layers'
+class MockTaskType(Enum):
+    FINAL = str(test_layers_dir)
 
 
 @patch(
-    'qgreenland.util.qgis.layer.get_final_layer_filepath',
-    new=MagicMock(return_value=mock_raster_path),
-)
-@patch(
-    'qgreenland.util.misc.get_final_layer_filepath',
-    new=MagicMock(return_value=mock_raster_path),
+    'qgreenland.util.misc.TaskType',
+    new=MockTaskType,
 )
 def test_create_raster_map_layer(setup_teardown_qgis_app, raster_layer_cfg):
     result = qgl.make_map_layer(raster_layer_cfg)
@@ -26,7 +25,8 @@ def test_create_raster_map_layer(setup_teardown_qgis_app, raster_layer_cfg):
     assert isinstance(result, qgc.QgsRasterLayer)
 
     # Has the expected path to the data on disk.
-    assert result.source() == str(mock_raster_path)
+    expected_raster_path = test_layers_dir / 'group' / 'subgroup' / 'example.tif'
+    assert result.source() == str(expected_raster_path)
 
     # With the expected shape.
     result_shape = (result.dataProvider().xSize(), result.dataProvider().ySize())
