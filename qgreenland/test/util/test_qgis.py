cf798183a45800cee24e3ff07e4501fc4944b1a0
Matt Fisher <matt.fisher@nsidc.org>
2021-08-12 10:34:45 -0600

    Update qgis util tests

--- qgreenland/test/util/test_qgis.py
+++ qgreenland/test/util/test_qgis.py
@@ -1,27 +1,32 @@
-import copy
+# import copy
 import os
+from pathlib import Path
+from unittest.mock import MagicMock, patch
 
 import qgis.core as qgc
 
 from qgreenland.constants import PACKAGE_DIR
-from qgreenland.util import qgis as _qgis
+from qgreenland.util.qgis import layer as qgl
+
+mock_raster_path = Path(PACKAGE_DIR, 'test', 'data', 'example.tif')
 
 
+@patch(
+    'qgreenland.util.qgis.layer.get_final_layer_filepath',
+    new=MagicMock(return_value=mock_raster_path),
+)
+@patch(
+    'qgreenland.util.misc.get_final_layer_filepath',
+    new=MagicMock(return_value=mock_raster_path),
+)
 def test_create_raster_map_layer(setup_teardown_qgis_app, raster_layer_cfg):
-    mock_raster_path = os.path.join(
-        PACKAGE_DIR,
-        'test',
-        'data',
-        'example.tif'
-    )
-
-    result = _qgis.create_raster_map_layer(mock_raster_path, raster_layer_cfg)
+    result = qgl.make_map_layer(raster_layer_cfg)
 
     # Assert that the result is a a raster layer
     assert isinstance(result, qgc.QgsRasterLayer)
 
     # Has the expected path to the data on disk.
-    assert result.source() == mock_raster_path
+    assert result.source() == str(mock_raster_path)
 
     # With the expected shape.
     result_shape = (result.dataProvider().xSize(), result.dataProvider().ySize())
@@ -33,73 +38,3 @@ def test_create_raster_map_layer(setup_teardown_qgis_app, raster_layer_cfg):
     assert True
 
     del result
-
-
-def test__add_layer_metadata(setup_teardown_qgis_app, raster_layer_cfg):
-    mock_raster_path = os.path.join(
-        PACKAGE_DIR,
-        'test',
-        'data',
-        'example.tif'
-    )
-
-    mock_raster_layer = _qgis.create_raster_map_layer(mock_raster_path, raster_layer_cfg)
-
-    _qgis._add_layer_metadata(mock_raster_layer, raster_layer_cfg)
-
-    # The abstract gets set with the value returned by `qgis.build_abstract`.
-    assert mock_raster_layer.metadata().abstract() == \
-        _qgis.build_layer_abstract(raster_layer_cfg)
-
-    actual_title = mock_raster_layer.metadata().title()
-    expected_title = raster_layer_cfg.title
-    assert actual_title == expected_title
-
-    # Sets the spatial extent based on the the layer extent.
-    expected_extent = mock_raster_layer.extent()
-    # extent metadata contains both spatial and temporal elements. These are
-    # lists. We set one overall extent for the dataset, so take the first element.
-    meta_extent = mock_raster_layer.metadata().extent().spatialExtents()[0]
-    # The `expected_extent` is a QgsRectangle.
-    assert expected_extent == meta_extent.bounds.toRectangle()
-
-    del mock_raster_layer
-
-
-def test__build_dataset_description(raster_layer_cfg):
-    actual = _qgis._build_dataset_description(raster_layer_cfg)
-    expected = """Example Dataset
-
-Example abstract"""
-
-    assert actual == expected
-
-
-def __build_dataset_citation(raster_layer_cfg):
-    actual = _qgis._build_dataset_citation(raster_layer_cfg)
-    expected = """Citation:
-NSIDC 2020
-
-Citation URL:
-https://nsidc.org"""
-
-    assert actual == expected
-
-
-def test_build_abstract(raster_layer_cfg):
-    mock_cfg = copy.deepcopy(raster_layer_cfg)
-    actual = _qgis.build_layer_abstract(mock_cfg)
-    expected = """Example layer description
-
-=== Original Data Source ===
-Example Dataset
-
-Example abstract
-
-Citation:
-NSIDC 2020
-
-Citation URL:
-https://nsidc.org"""
-
-    assert actual == expected
