6afefaf205785691bd4ad03fc3bb790f834f2219
Trey Stafford <trey.stafford@nsidc.org>
2021-08-16 13:48:00 -0600

    Add provenance steps to `History` tab in layer properties

--- qgreenland/util/qgis/metadata.py
+++ qgreenland/util/qgis/metadata.py
@@ -38,7 +38,10 @@ def add_layer_metadata(map_layer: qgc.QgsMapLayer, layer_cfg: ConfigLayer) -> No
     layer_crs = map_layer.crs()
 
     qmd_template = Template(qmd_template_str)
+    provenance_list = [escape(step.provenance) for step in layer_cfg.steps] if layer_cfg.steps else []
+
     rendered_qmd = qmd_template.render(
+        provenance_list=provenance_list,
         abstract=abstract,
         title=layer_cfg.title,
         crs_proj4_str=layer_crs.toProj4(),
