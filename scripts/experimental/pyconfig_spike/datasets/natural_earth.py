7262e940bfb189f0602e458346b1cc9c0d432897
Trey Stafford <trey.stafford@nsidc.org>
2021-08-20 15:28:44 -0600

    WIP init

---
+++ scripts/experimental/pyconfig_spike/datasets/natural_earth.py
@@ -0,0 +1,21 @@
+from qgreenland.models.config.dataset import ConfigDataset
+
+background = ConfigDataset(
+    id='background',
+    assets={
+        # 'high_res': high_res_asset,
+        'high_res': {
+            'id': 'high_res',
+            'type': 'http',
+            'urls': ['https://naciscdn.org/naturalearth/10m/raster/NE2_HR_LC_SR_W.zip'],
+        }
+    },
+    metadata={
+        'title': 'Natural Earth II with Shaded Relief and Water (1:10m)',
+        'abstract': 'Natural Earth II (Public Domain)',
+        'citation': {
+            'text': 'Made with Natural Earth',
+            'url': 'https://github.com/nvkelso/natural-earth-vector/blob/master/LICENSE.md',
+        }
+    }
+)
---
+++ scripts/experimental/pyconfig_spike/layers/background.py
@@ -0,0 +1,19 @@
+from qgreenland.models.config.layer import ConfigLayer
+
+from scripts.experimental.pyconfig_spike.datasets.natural_earth import background as background_dataset
+
+background = ConfigLayer(
+    id='background',
+    title='Background (500m)',
+    description='Stylized shaded-relief map for providing a general sense of geography.',
+    hierarchy=['Basemaps'],
+    in_package=True,
+    show=True,
+    input={
+        'dataset': background_dataset,
+        'asset': background_dataset.assets['high_res'],
+    },
+    steps=[],
+)
+
+print(background)
