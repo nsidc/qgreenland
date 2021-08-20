5c938bcfc2bde1b942857077dac097b2c6abb756
Trey Stafford <trey.stafford@nsidc.org>
2021-08-20 15:57:17 -0600

    WIP warp_and_cut step template

--- scripts/experimental/pyconfig_spike/layers/background.py
+++ scripts/experimental/pyconfig_spike/layers/background.py
@@ -1,6 +1,8 @@
 from qgreenland.models.config.layer import ConfigLayer
+from qgreenland.models.config.step import ConfigLayerCommandStep
 
 from scripts.experimental.pyconfig_spike.datasets.natural_earth import background as background_dataset
+from scripts.experimental.pyconfig_spike.step_templates.warp_and_cut import warp_and_cut
 
 background = ConfigLayer(
     id='background',
@@ -13,7 +15,21 @@ background = ConfigLayer(
         'dataset': background_dataset,
         'asset': background_dataset.assets['high_res'],
     },
-    steps=[],
+    steps=[
+        ConfigLayerCommandStep(
+            type='command',
+            args=['unzip', '{input_dir}/*.zip', '-d', '{output_dir}'],
+        ),
+        *warp_and_cut(
+            input_file='{input_dir}/NE2_HR_LC_SR_W/NE2_HR_LC_SR_W.tif',
+            output_file='{output_dir}/warped_and_cut.tif',
+            xRes=500,
+            yRes=500,
+            # TODO import project config and access correct boundary.
+            target_extent='-5774572.727595 -5774572.727595 5774572.727595 5774572.727595',
+            cut_file='{assets_dir}/latitude_shape_40_degrees.geojson',
+        ),
+    ],
 )
 
 print(background.json(indent=4))
---
+++ scripts/experimental/pyconfig_spike/step_templates/warp_and_cut.py
@@ -0,0 +1,64 @@
+from typing import List
+
+from qgreenland.models.config.step import AnyStep, ConfigLayerCommandStep
+
+
+def warp_and_cut(
+        *,
+        # TODO: think about how to require all step template functions to take
+        # input_file, output_file.
+        input_file,
+        output_file,
+        xRes,
+        yRes,
+        target_extent,
+        cut_file
+) -> List[AnyStep]:
+    reproject = ConfigLayerCommandStep(
+        type='command',
+        args=[
+            'gdalwarp',
+            '-t_srs',  # dstCRS
+            # TODO: Reference project SRS
+            'EPSG:3413',
+            '-tr',  # xRes=500, yRes=500
+            f'{xRes}',
+            f'{yRes}',
+            '-te',
+            f'{target_extent}',
+            '-dstnodata',  # dstNodata
+            '0',
+            '-wo',  # warpOptions=['SOURCE_EXTRA=100', 'SAMPLE_GRID=YES']
+            'SOURCE_EXTRA=100',
+            '-wo',
+            'SAMPLE_GRID=YES',
+            # What about using dedicated keys for `input_file` and `output_file` so
+            # the command itself can reference that slug. If either is repeated in5
+            # the command, this will help avoid mistakes.
+            f'{input_file}',  # <--- Input
+            '{output_dir}/warped.tif',  # <--- Output
+        ]
+    )
+
+    clip = ConfigLayerCommandStep(
+        # TODO: Do we have to spec 'command' here?
+        type='command',
+        args=[
+            'gdalwarp',
+            '-cutline',  # CutlineDSName
+             # TODO: Abstract boundaries... as slugs? e.g.:
+             #     {boundaries.background.filepath}
+            f'{cut_file}',
+            '-crop_to_cutline',  # CropToCutline=True
+            '-co',  # creationOptions=['COMPRESS=DEFLATE']
+            'COMPRESS=DEFLATE',
+            '{input_dir}/warped.tif',  # <--- Input
+            f'{output_file}',  # <--- Output
+        ]
+        
+    )
+
+    return [
+        reproject,
+        clip
+    ]
