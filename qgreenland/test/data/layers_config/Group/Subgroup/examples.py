384728f100c4704402c58685865bb99a8d1737d6
Trey Stafford <trey.stafford@nsidc.org>
2021-08-26 09:56:18 -0600

    Simplify imports of example layers in mock layer config

--- qgreenland/test/data/layers_config/Group/Subgroup/examples.py
+++ qgreenland/test/data/layers_config/Group/Subgroup/examples.py
@@ -1,2 +1,4 @@
-from qgreenland.test.conftest import mock_online_layer_cfg as example_online
-from qgreenland.test.conftest import mock_raster_layer_cfg as example_raster
+from qgreenland.test.conftest import (
+    mock_online_layer_cfg,
+    mock_raster_layer_cfg
+)
