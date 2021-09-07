d378bc92735ed0e3331971ae86055039cf883ab7
Matt Fisher <matt.fisher@nsidc.org>
2021-09-07 17:23:18 -0600

    Cleanup OBE comment

--- qgreenland/config/layers/__settings__.py
+++ qgreenland/config/layers/__settings__.py
@@ -1,8 +1,6 @@
 from qgreenland.models.config.layer_group import RootGroupSettings
 
 
-# NOTE: Only "order" can be defined in the root group. Create a
-# RootGroupSettings model for this?
 settings = RootGroupSettings(
     order=[
         'Terrain Models',
