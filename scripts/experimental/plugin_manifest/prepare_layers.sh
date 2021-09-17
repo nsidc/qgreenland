5d7d8ac5a509a837d228502480aeaa6101654ff8
Matt Fisher <matt.fisher@nsidc.org>
2021-09-17 17:02:42 -0600

    Update sample docker server build tooling

--- scripts/experimental/plugin_manifest/prepare_layers.sh
+++ scripts/experimental/plugin_manifest/prepare_layers.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 # We currently don't write out the layers in the way they need to be organized
 # for public hosting.
-LUIGIDIR='/share/appdata/qgreenland/luigi-wip'
-SOURCE="$LUIGIDIR/wip"
+SRC_DIR='/share/appdata/qgreenland/release/layers'
 DEST='./public'
 
 mkdir -p $DEST
 
-for layer_dir in "$SOURCE"/*; do
-    layer_id=$(basename "$layer_dir")
-    final_data_dir="$(find "$layer_dir" -maxdepth 1 | sort | tail -n 1)"
-
-    cp -R "$final_data_dir" "$DEST/$layer_id"
-done
-
-cp $LUIGIDIR/QGreenland/manifest.json $DEST/.
+cp -R $SRC_DIR/* $DEST/.
