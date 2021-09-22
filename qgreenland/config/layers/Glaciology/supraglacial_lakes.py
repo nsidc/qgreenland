b37cc457df9ac32803883ed784400c3a9e778677
Matt Fisher <matt.fisher@nsidc.org>
2021-09-22 16:05:39 -0600

    Update jakobshavn config from migration template

--- qgreenland/config/layers/Glaciology/supraglacial_lakes.py
+++ qgreenland/config/layers/Glaciology/supraglacial_lakes.py
@@ -1,3 +1,4 @@
+# TODO
 from qgreenland.config.datasets.esa_cci import (
     esa_cci_supraglacial_lakes as dataset,
 )
@@ -9,35 +10,17 @@ from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
 supraglacial_lakes = ConfigLayer(
     id='jakobshavn_supraglacial_lakes',
     title='Sermeq Kujalleq/Jakobshavn supraglacial lakes 2019',
-    description="""Supraglacial lake delineation on Sermeq Kujalleq/Jakobshavn
-for 2019/05/01 and 2019/10/01 generated using Sentinel-2 satellite data.""",
+    description=(
+        """Supraglacial lake delineation on Sermeq Kujalleq/Jakobshavn for
+        2019/05/01 and 2019/10/01 generated using Sentinel-2 satellite data."""
+    ),
     tags=['water'],
     style='supraglacial_lakes',
     input=ConfigLayerInput(
         dataset=dataset,
         asset=dataset.assets['only'],
     ),
-    steps=[],
+    steps=[
+        # TODO
+    ],
 )
-
-# MIGRATION:
-# from qgreenland.config.datasets.XXXX import XXXX as dataset
-# from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
-#
-#
-# jakobshavn_supraglacial_lakes = ConfigLayer(
-#     id='jakobshavn_supraglacial_lakes',
-#     title='Sermeq Kujalleq/Jakobshavn supraglacial lakes 2019',
-#     description=(
-#         """Supraglacial lake delineation on Sermeq Kujalleq/Jakobshavn for
-#         2019/05/01 and 2019/10/01 generated using Sentinel-2 satellite data."""
-#     ),
-#     tags=[],
-#     style='XXXX',
-#     input=ConfigLayerInput(
-#         dataset=dataset,
-#         asset=dataset.assets['XXXX'],
-#     ),
-#     steps=[
-#     ],
-# )
