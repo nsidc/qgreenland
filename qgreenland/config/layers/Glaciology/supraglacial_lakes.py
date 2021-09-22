09d6498fe8ee23a5764d3ccbd4ab78ca971b0264
Matt Fisher <matt.fisher@nsidc.org>
2021-09-21 18:48:12 -0600

    Migrate supraglacial lakes layer into a comment

--- qgreenland/config/layers/Glaciology/supraglacial_lakes.py
+++ qgreenland/config/layers/Glaciology/supraglacial_lakes.py
@@ -19,3 +19,26 @@ for 2019/05/01 and 2019/10/01 generated using Sentinel-2 satellite data.""",
     ),
     steps=[],
 )
+
+# MIGRATION:
+# from qgreenland.config.datasets.XXXX import XXXX as dataset
+# from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
+# 
+# 
+# jakobshavn_supraglacial_lakes = ConfigLayer(
+#     id='jakobshavn_supraglacial_lakes',
+#     title='Sermeq Kujalleq/Jakobshavn supraglacial lakes 2019',
+#     description=(
+#         """Supraglacial lake delineation on Sermeq Kujalleq/Jakobshavn for
+#         2019/05/01 and 2019/10/01 generated using Sentinel-2 satellite data."""
+#     ),
+#     tags=[],
+#     style='XXXX',
+#     input=ConfigLayerInput(
+#         dataset=dataset,
+#         asset=dataset.assets['XXXX'],
+#     ),
+#     steps=[
+#     ],
+# )
+# 
