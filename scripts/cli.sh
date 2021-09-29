603793290b130b9cb6f014b0b9a975dbf45177e2
Matt Fisher <matt.fisher@nsidc.org>
2021-09-29 14:36:56 -0600

    Cleanup alternate ways of running QGreenland

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [[ "$1" == "run" || "$1" == "cleanup" ]]; then
+    echo "The 'run' and 'cleanup' commands should be run from \`container_cli.sh\`."
+    exit 1
+fi
+
 THIS_DIR="$( readlink -f "$( dirname "${BASH_SOURCE[0]}" )")"
 cd "$THIS_DIR"/..
 
