12316e029f582cf34befb5ec7e95a3386a89c305
Trey Stafford <trey.stafford@nsidc.org>
2021-09-29 14:23:10 -0600

    Fixup dir name, add ordering for seaice groups & layers

---
+++ qgreenland/config/layers/Sea ice/Weekly age (12.5km)/September (min extent)/__settings__.py
@@ -0,0 +1,10 @@
+from qgreenland.config.helpers.layers.sea_ice_age import seaice_age_layers
+from qgreenland.models.config.layer_group import LayerGroupSettings
+
+
+settings = LayerGroupSettings(
+    order=[
+        f'layers.py:seaice_minimum_age_{year}'
+        for year in seaice_age_layers.keys()
+    ],
+)
