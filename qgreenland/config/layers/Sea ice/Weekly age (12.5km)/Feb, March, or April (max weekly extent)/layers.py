d963ab67ca3e4036d9f3178a7b0d428d15155708
Trey Stafford <trey.stafford@nsidc.org>
2021-09-30 15:57:57 -0600

    Fixup typo

--- qgreenland/config/layers/Sea ice/Weekly age (12.5km)/Feb, March, or April (max weekly extent)/layers.py
+++ qgreenland/config/layers/Sea ice/Weekly age (12.5km)/Feb, March, or April (max weekly extent)/layers.py
@@ -1,7 +1,7 @@
 from qgreenland.config.helpers.layers.sea_ice_age import create_sea_ice_age_layers
 
 
-# TODO: this is weekly data, so does it relaly belong in a group called
+# TODO: this is weekly data, so does it really belong in a group called
 # 'Feb or March'?
 
 seaice_minimum_age_layers = create_sea_ice_age_layers('maximum')
