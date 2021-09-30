ea0ce7ea03e83b43c62a863bc058f20450ba141a
Trey Stafford <trey.stafford@nsidc.org>
2021-09-30 15:23:37 -0600

    Expand the sea ice age and sea ice concentration groups
    matches v1

--- qgreenland/config/layers/Sea ice/Monthly mean concentration (25 km)/__settings__.py
+++ qgreenland/config/layers/Sea ice/Monthly mean concentration (25 km)/__settings__.py
@@ -2,9 +2,5 @@ from qgreenland.models.config.layer_group import LayerGroupSettings
 
 
 settings = LayerGroupSettings(
-    order=[
-        'September (min weekly extent)',
-        'Feb, March, or April (max weekly extent)',
-    ],
     expanded=True,
 )
