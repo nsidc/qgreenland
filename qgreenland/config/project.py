b662ecb22d368858db92ab2d9d8cb9a1434ca758
Trey Stafford <trey.stafford@nsidc.org>
2021-10-01 15:58:22 -0600

    use `ASSETS_DIR` constant for boundary filepaths in `project` cfg

--- qgreenland/config/project.py
+++ qgreenland/config/project.py
@@ -1,14 +1,15 @@
 from qgreenland.config.constants import PROJECT_CRS
+from qgreenland.constants import ASSETS_DIR
 from qgreenland.models.config.project import ConfigProject
 
 project = ConfigProject(
     crs=PROJECT_CRS,
     boundaries={
         'background': {
-            'filepath': '{assets_dir}/latitude_shape_40_degrees.geojson',  # noqa: FS003
+            'filepath': f'{ASSETS_DIR}/latitude_shape_40_degrees.geojson',  # noqa: FS003
         },
         'data': {
-            'filepath': '{assets_dir}/greenland_rectangle.geojson',  # noqa: FS003
+            'filepath': f'{ASSETS_DIR}/greenland_rectangle.geojson',  # noqa: FS003
         },
     },
 )
