c9f1884257c18732282ae18dd6032def920a3ac2
Trey Stafford <trey.stafford@nsidc.org>
2021-10-01 11:01:36 -0600

    Add TODO

--- qgreenland/config/layers/Internet-required data/__settings__.py
+++ qgreenland/config/layers/Internet-required data/__settings__.py
@@ -4,6 +4,8 @@ from qgreenland.models.config.layer_group import LayerGroupSettings
 settings = LayerGroupSettings(
     order=[
         'online.py:image_mosaic_2019',
+        # TODO: why is this layer failing?
+        #   `Invalid QgsMapLayer created for layer g02135_polyline_n`
         # 'online.py:g02135_polyline_n',
     ],
 )
