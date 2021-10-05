072ab6037c341d05a4c6adb286fd0baacb9f7033
Trey Stafford <trey.stafford@nsidc.org>
2021-10-05 16:28:08 -0600

    More tests for runtime vars

--- qgreenland/test/util/test_runtime_vars.py
+++ qgreenland/test/util/test_runtime_vars.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 import pytest
 
 from qgreenland.constants import ASSETS_DIR
 from qgreenland.exceptions import QgrInterpolationError
+from qgreenland.models.config.step import ConfigLayerCommandStep
 from qgreenland.util.runtime_vars import EvalFilePath, EvalPath, EvalStr
 
 
@@ -10,6 +13,15 @@ def test_eval_path():
     expected = ASSETS_DIR / 'arctic_circle.geojson'
 
     assert eval_path.eval() == expected
+
+    # Assert eval w/ kwarg works as expected.
+    input_dir = '/foo/bar'
+    expected = Path(f'{input_dir}/baz.tif')
+    assert EvalPath('{input_dir}/baz.tif').eval(input_dir=input_dir) == expected
+
+    # Assert eval w/ unexpected kwarg
+    with pytest.raises(TypeError):
+        EvalPath('{foo}/baz.tif').eval(foo='bar')
 
 
 def test_eval_file_path():
@@ -30,3 +42,15 @@ def test_eval_str():
 
     # concatenation
     assert isinstance(EvalStr('{input_dir}/') + 'foobar.txt', EvalStr)
+
+
+def test_config_layer_cmd_step_args_validation():
+    command_step = ConfigLayerCommandStep(
+        args=[
+            'foo',
+            'bar',
+            'baz',
+        ],
+    )
+
+    assert all(isinstance(arg, EvalStr) for arg in command_step.args)
