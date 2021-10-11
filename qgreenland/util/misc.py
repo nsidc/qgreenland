e04db07f5113afeaf28bc3d1b82a9cc22c882701
Trey Stafford <trey.stafford@nsidc.org>
2021-10-11 13:22:06 -0600

    When fetching files, spec a `User-Agent` header of `QGreenland`
    Attempt to fetch the natural earth coastlines layer results in a `406: Not
    Allowed` when using `requests` without specifying a user agent.

--- qgreenland/util/misc.py
+++ qgreenland/util/misc.py
@@ -75,7 +75,12 @@ def fetch_and_write_file(url, *, output_dir, session=None, verify=True):  # noqa
         if not session:
             session = create_earthdata_authenticated_session(hosts=[url], verify=verify)
 
-        with session.get(url, timeout=REQUEST_TIMEOUT, stream=True) as resp:
+        with session.get(
+                url,
+                timeout=REQUEST_TIMEOUT,
+                stream=True,
+                headers={'User-Agent': 'QGreenland'},
+        ) as resp:
 
             # Try to extract the filename from the `content-disposition` header
             if (
