d75218f28c2d19d29f98d1dde5594b99a3bff8bd
Trey Stafford <trey.stafford@nsidc.org>
2021-10-13 15:56:24 -0600

    migrate racmo wind speed layer

--- qgreenland/config/layers/Regional climate models/RACMO model output/layers.py
+++ qgreenland/config/layers/Regional climate models/RACMO model output/layers.py
@@ -1,5 +1,8 @@
 from qgreenland.config.datasets.racmo import racmo_qgreenland_jan2021 as dataset
+from qgreenland.config.helpers.steps.decompress import decompress_step
+from qgreenland.config.helpers.steps.warp_and_cut import warp_and_cut
 from qgreenland.config.helpers.steps.zipped_vector import zipped_vector
+from qgreenland.config.project import project
 from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
 
 
@@ -21,6 +24,35 @@ racmo_wind_vectors = ConfigLayer(
             input_file='{input_dir}/RACMO_QGreenland_Jan2021.zip',
             output_file='{output_dir}/racmo_wind_vectors.gpkg',
             vector_filename='wind_vector_points.gpkg',
+            decompress_step_kwargs={
+                'decompress_contents_mask': 'wind_vector_points.gpkg',
+            },
+        ),
+    ],
+)
+
+racmo_wind_speed = ConfigLayer(
+    id='racmo_wind_speed',
+    title='Annual mean wind speed 1958-2019 (5km)',
+    description=(
+        """Averaged annual mean wind speed in meters per second from RACMO2.3p2
+        for the period 1958-2019."""
+    ),
+    tags=[],
+    style='racmo_wind_speed',
+    input=ConfigLayerInput(
+        dataset=dataset,
+        asset=dataset.assets['only'],
+    ),
+    steps=[
+        decompress_step(
+            input_file='{input_dir}/RACMO_QGreenland_Jan2021.zip',
+            decompress_contents_mask='magnitudes.nc',
+        ),
+        *warp_and_cut(
+            input_file='{input_dir}/magnitudes.nc',
+            output_file='{output_dir}/racmo_wind_speed.tif',
+            cut_file=project.boundaries['data'].filepath,
         ),
     ],
 )
