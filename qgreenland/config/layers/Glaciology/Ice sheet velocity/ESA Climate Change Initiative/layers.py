6c4a5390988bc1dad88da7932cc385ddee143a8d
Trey Stafford <trey.stafford@nsidc.org>
2021-10-15 14:59:49 -0600

    Migrate esa_cci_velocity_magnitude layer

---
+++ qgreenland/config/layers/Glaciology/Ice sheet velocity/ESA Climate Change Initiative/layers.py
@@ -0,0 +1,44 @@
+from qgreenland.config.project import project
+from qgreenland.config.datasets.esa_cci import esa_cci_ice_sheet_velocity_20191214_20200131 as dataset
+from qgreenland.config.helpers.steps.build_overviews import build_overviews
+from qgreenland.config.helpers.steps.decompress import decompress_step
+from qgreenland.config.helpers.steps.warp_and_cut import warp_and_cut
+from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
+
+
+esa_cci_velocity_magnitude = ConfigLayer(
+    id='esa_cci_velocity_magnitude',
+    title='Land ice surface velocity 2019-2020 (250m)',
+    description=(
+        """Magnitude of horizontal ice velocity in meters per day. Data is not
+        masked to ice sheet boundary."""
+    ),
+    tags=[],
+    style='esa_cci_velocity',
+    input=ConfigLayerInput(
+        dataset=dataset,
+        asset=dataset.assets['only'],
+    ),
+    steps=[
+        decompress_step(
+            input_file='{input_dir}/greenland_iv_250m_s1_20191214_20200131_v1_3.zip',
+            decompress_contents_mask=(
+                'greenland_iv_250m_s1_20191214_20200131_v1_3'
+                '/greenland_iv_250m_s1_20191214_20200131_v1_3.nc'
+            ),
+        ),
+        *warp_and_cut(
+            input_file=(
+                'NETCDF:{input_dir}/greenland_iv_250m_s1_20191214_20200131_v1_3/'
+                'greenland_iv_250m_s1_20191214_20200131_v1_3.nc'
+                ':land_ice_surface_velocity_magnitude'
+            ),
+            output_file='{output_dir}/magnitude.tif',
+            cut_file=project.boundaries['data'].filepath,
+        ),
+        *build_overviews(
+            input_file='{input_dir}/magnitude.tif',
+            output_file='{output_dir}/magnitude.tif',
+        ),
+    ],
+)
