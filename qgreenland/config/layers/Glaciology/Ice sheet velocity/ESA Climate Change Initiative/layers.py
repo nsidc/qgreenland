59a95357eef8a41922cf88608f0e4dd6c6b01402
Trey Stafford <trey.stafford@nsidc.org>
2021-10-15 15:15:37 -0600

    Add vertical velocity layer

--- qgreenland/config/layers/Glaciology/Ice sheet velocity/ESA Climate Change Initiative/layers.py
+++ qgreenland/config/layers/Glaciology/Ice sheet velocity/ESA Climate Change Initiative/layers.py
@@ -6,39 +6,62 @@ from qgreenland.config.helpers.steps.warp_and_cut import warp_and_cut
 from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
 
 
-esa_cci_velocity_magnitude = ConfigLayer(
-    id='esa_cci_velocity_magnitude',
-    title='Land ice surface velocity 2019-2020 (250m)',
-    description=(
-        """Magnitude of horizontal ice velocity in meters per day. Data is not
-        masked to ice sheet boundary."""
-    ),
-    tags=[],
-    style='esa_cci_velocity',
-    input=ConfigLayerInput(
-        dataset=dataset,
-        asset=dataset.assets['only'],
-    ),
-    steps=[
-        decompress_step(
-            input_file='{input_dir}/greenland_iv_250m_s1_20191214_20200131_v1_3.zip',
-            decompress_contents_mask=(
-                'greenland_iv_250m_s1_20191214_20200131_v1_3'
-                '/greenland_iv_250m_s1_20191214_20200131_v1_3.nc'
+_esa_cci_velocity_params = {
+    'esa_cci_velocity_magnitude': {
+        'title': 'Land ice surface velocity 2019-2020 (250m)',
+        'description': (
+            """Magnitude of horizontal ice velocity in meters per day. Data is
+            not masked to ice sheet boundary."""
+        ),
+        'variable': 'land_ice_surface_velocity_magnitude',
+        'style': 'esa_cci_velocity',
+    },
+    'esa_cci_velocity_vertical': {
+        # TODO: should this title aslo indicate 2019-2020?
+        'title': 'Land ice surface vertical velocity (250m)',
+        'description': (
+            """Vertical ice velocity in meters per day. Data is not masked to
+            ice sheet boundary."""
+        ),
+        'variable': 'land_ice_surface_vertical_velocity',
+        'style': 'vertical_velocity',
+    },
+}
+
+
+layers = [
+    ConfigLayer(
+        id=layer_id,
+        title=params['title'],
+        description=params['description'],
+        tags=[],
+        style=params['style'],
+        input=ConfigLayerInput(
+            dataset=dataset,
+            asset=dataset.assets['only'],
+        ),
+        steps=[
+            decompress_step(
+                input_file='{input_dir}/greenland_iv_250m_s1_20191214_20200131_v1_3.zip',
+                decompress_contents_mask=(
+                    'greenland_iv_250m_s1_20191214_20200131_v1_3'
+                    '/greenland_iv_250m_s1_20191214_20200131_v1_3.nc'
+                ),
             ),
-        ),
-        *warp_and_cut(
-            input_file=(
-                'NETCDF:{input_dir}/greenland_iv_250m_s1_20191214_20200131_v1_3/'
-                'greenland_iv_250m_s1_20191214_20200131_v1_3.nc'
-                ':land_ice_surface_velocity_magnitude'
+            *warp_and_cut(
+                input_file=(
+                    'NETCDF:{input_dir}/greenland_iv_250m_s1_20191214_20200131_v1_3/'
+                    'greenland_iv_250m_s1_20191214_20200131_v1_3.nc'
+                    f":{params['variable']}"
+                ),
+                output_file='{output_dir}/' + f'{layer_id}.tif',
+                cut_file=project.boundaries['data'].filepath,
             ),
-            output_file='{output_dir}/magnitude.tif',
-            cut_file=project.boundaries['data'].filepath,
-        ),
-        *build_overviews(
-            input_file='{input_dir}/magnitude.tif',
-            output_file='{output_dir}/magnitude.tif',
-        ),
-    ],
-)
+            *build_overviews(
+                input_file='{input_dir}/' + f'{layer_id}.tif',
+                output_file='{output_dir}/' + f'{layer_id}.tif',
+            ),
+        ],
+    )
+    for layer_id, params in _esa_cci_velocity_params.items()
+]
