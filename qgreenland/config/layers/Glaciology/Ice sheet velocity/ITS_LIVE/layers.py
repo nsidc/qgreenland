cfbff8038985a98e380fbe680cadf0b5989bd852
Trey Stafford <trey.stafford@nsidc.org>
2021-10-15 14:17:26 -0600

    Add ice sheet velocity mosaic error layer

--- qgreenland/config/layers/Glaciology/Ice sheet velocity/ITS_LIVE/layers.py
+++ qgreenland/config/layers/Glaciology/Ice sheet velocity/ITS_LIVE/layers.py
@@ -6,36 +6,52 @@ from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
 from qgreenland.models.config.step import ConfigLayerCommandStep
 
 
-velocity_mosaic = ConfigLayer(
-    id='velocity_mosaic',
-    title='Ice sheet velocity (120m)',
-    description=(
-        """Magnitude of velocity in meters per year."""
-    ),
-    tags=[],
-    style='ice_sheet_velocity',
-    input=ConfigLayerInput(
-        dataset=dataset,
-        asset=dataset.assets['only'],
-    ),
-    steps=[
-        ConfigLayerCommandStep(
-            args=[
-                'gdal_calc.py',
-                '--calc="A*B"',
-                '--outfile={output_dir}/masked_velocity_mosaic.tif',
-                '-A', 'NETCDF:{input_dir}/GRE_G0120_0000.nc:v',
-                '-B', 'NETCDF:{input_dir}/GRE_G0120_0000.nc:ice',
-            ],
+_masked_velocity_mosaic_params = {
+    'velocity_mosaic': {
+        'title': 'Ice sheet velocity (120m)',
+        'description': 'Magnitude of velocity in meters per year.',
+        'variable': 'v',
+        'style': 'ice_sheet_velocity'
+    },
+    'velocity_mosaic_error': {
+        'title': 'Velocity error (120m)',
+        'description': 'Magnitude of velocity error in meters per year.',
+        'variable': 'v_err',
+        'style': 'ice_sheet_velocity_error',
+    },
+}
+
+masked_velocity_mosaic_layers = [
+    ConfigLayer(
+        id=layer_id,
+        title=params['title'],
+        description=params['description'],
+        tags=[],
+        style=params['style'],
+        input=ConfigLayerInput(
+            dataset=dataset,
+            asset=dataset.assets['only'],
         ),
-        *warp_and_cut(
-            input_file='{input_dir}/masked_velocity_mosaic.tif',
-            output_file='{output_dir}/velocity_mosaic.tif',
-            cut_file=project.boundaries['data'].filepath,
-        ),
-        *build_overviews(
-            input_file='{input_dir}/velocity_mosaic.tif',
-            output_file='{output_dir}/velocity_mosaic.tif',
-        ),
-    ],
-)
+        steps=[
+            ConfigLayerCommandStep(
+                args=[
+                    'gdal_calc.py',
+                    '--calc="A*B"',
+                    '--outfile={output_dir}/' + f'masked_{layer_id}.tif',
+                    '-A', 'NETCDF:{input_dir}/GRE_G0120_0000.nc:v',
+                    '-B', 'NETCDF:{input_dir}/GRE_G0120_0000.nc:ice',
+                ],
+            ),
+            *warp_and_cut(
+                input_file='{input_dir}/' + f'masked_{layer_id}.tif',
+                output_file='{output_dir}/' + f'{layer_id}.tif',
+                cut_file=project.boundaries['data'].filepath,
+            ),
+            *build_overviews(
+                input_file='{input_dir}/' + f'{layer_id}.tif',
+                output_file='{output_dir}/' + f'{layer_id}.tif',
+            ),
+        ],
+    )
+    for layer_id, params in _masked_velocity_mosaic_params.items()
+]
