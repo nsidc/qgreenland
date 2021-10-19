efe1a38554164c391e5c2a545c7796cc9bc2f44d
Hope Simonoko <hope.simonoko@colorado.edu>
2021-10-19 15:26:05 -0600

    fix linting errors

--- qgreenland/config/layers/Glaciology/Glacier terminus positions 2000-2017/glacier_terminus.py
+++ qgreenland/config/layers/Glaciology/Glacier terminus positions 2000-2017/glacier_terminus.py
@@ -1,33 +1,36 @@
-from qgreenland.config.datasets.glacier_terminus import datasets as dataset
+from qgreenland.config.datasets.glacier_terminus import glacier_terminus as dataset
 from qgreenland.config.helpers.steps.ogr2ogr import ogr2ogr
 from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
 
 
+START = 2005
+END = 2017
+
 LAYER_YEARS = [
     (2000, 2001),
     *[
-        (start_year, start_year+1)
-        for year in range(2005, 2017+1)
+        (START, START + 1)
+        for year in range(2005, 2017 + 1)
     ],
-] 
+]
 
 layers = [
     ConfigLayer(
-        id = f'glacier_terminus_{start_year}_{end_year}',
-        title  = f'Glacier termini {start_year} to {end_year}',
-        description = '',
-        tags = [],
+        id=f'glacier_terminus_{START}_{END}',
+        title=f'Glacier termini {START} to {END}',
+        description='',
+        tags=[],
         input=ConfigLayerInput(
             dataset=dataset,
-            asset=dataset.assets[f'{start_year}_{end_year}'],
+            asset=dataset.assets[f'{START}_{END}'],
         ),
         steps=[
             *ogr2ogr(
-                input_file='{input_dir}/glacier_terminus.' + start_year + '_' + end_year,
+                input_file='{input_dir}/glacier_terminus.' + str(START) + '_' + str(END),
                 output_file='{output_dir}/boundary.gpkg',
             ),
         ],
 
     )
-    for (start_year, end_year) in LAYER_YEARS
+    for (START, END) in LAYER_YEARS
 ]
--- qgreenland/config/layers/Glaciology/Glacier terminus positions 2000-2017/glacier_terminus_ids.py
+++ qgreenland/config/layers/Glaciology/Glacier terminus positions 2000-2017/glacier_terminus_ids.py
@@ -1,14 +1,12 @@
-from qgreenland.config.datasets.glacier_terminus import datasets as dataset
-from qgreenland.config.helpers.ogr2ogr import ogr2ogr
+from qgreenland.config.datasets.glacier_terminus import glacier_terminus as dataset
+from qgreenland.config.helpers.steps.ogr2ogr import ogr2ogr
 from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
 
 
 glacier_terminus_glacier_ids = ConfigLayer(
     id='glacier_terminus_glacier_ids',
     title='Glacier IDs',
-    description=(
-        """"""
-    ),
+    description='',
     tags=[],
     style='glacier_ids',
     input=ConfigLayerInput(
@@ -17,8 +15,8 @@ glacier_terminus_glacier_ids = ConfigLayer(
     ),
     steps=[
         *ogr2ogr(
-                input_file='{input_dir}/glacier_terminus.glacier_ids,
-                output_file='{output_dir}/boundary.gpkg',
-            ),
+            input_file='{input_dir}/glacier_terminus.glacier_ids',
+            output_file='{output_dir}/boundary.gpkg',
+        ),
     ],
 )
