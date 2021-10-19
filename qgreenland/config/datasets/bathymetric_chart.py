02e6dc5b0437b585cfc58d1b102c1be9f311d24a
Trey Stafford <trey.stafford@nsidc.org>
2021-10-19 10:53:08 -0600

    WIP migrate bathymetric chart layer

---
+++ qgreenland/config/datasets/bathymetric_chart.py
@@ -0,0 +1,32 @@
+from qgreenland.models.config.asset import ConfigDatasetHttpAsset
+from qgreenland.models.config.dataset import ConfigDataset
+
+
+bathymetric_chart = ConfigDataset(
+    id='bathymetric_chart',
+    assets=[
+        ConfigDatasetHttpAsset(
+            id='only',
+            urls=[
+                'https://www.bodc.ac.uk/data/open_download/ibcao/ibcao_v4_400m_ice/cfnetcdf/',
+            ],
+        ),
+    ],
+    metadata={
+        'title': 'Bathymetric Chart of the Arctic Ocean (IBCAO)',
+        'abstract': (
+            """The goal of the IBCAO initiative is to develop a digital database
+            that contains all available bathymetric data north of 64° North, for
+            use by mapmakers, researchers, institutions, and others whose work
+            requires a detailed and accurate knowledge of the depth and the
+            shape of the Arctic seabed."""
+        ),
+        'citation': {
+            'text': (
+                """GEBCO Compilation Group (2020) GEBCO 2020 Grid
+                (doi:10.5285/a29c5465-b138-234d-e053-6c86abc040b9)"""
+            ),
+            'url': 'https://www.gebco.net/data_and_products/gridded_bathymetry_data/arctic_ocean/',
+        },
+    },
+)
---
+++ qgreenland/config/layers/Oceanography/bathymetric_chart.py
@@ -0,0 +1,34 @@
+from qgreenland.config.datasets.bathymetric_chart import bathymetric_chart as dataset
+from qgreenland.config.helpers.steps.warp_and_cut import warp_and_cut
+from qgreenland.config.project import project
+from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
+
+
+bathymetric_chart = ConfigLayer(
+    id='bathymetric_chart',
+    title='Bathymetric chart of the Arctic Ocean (400m)',
+    description=(
+        """Bathymetric elevation in meters. Elevations can be assumed to be
+        relative to mean sea level. However, in some shallow water areas, the
+        grids include data from sources having a vertical datum other than mean
+        sea level."""
+    ),
+    tags=[],
+    style='ibcao_bathymetry',
+    input=ConfigLayerInput(
+        dataset=dataset,
+        asset=dataset.assets['only'],
+    ),
+    steps=[
+        *warp_and_cut(
+            input_file='NETCDF:{input_dir}/IBCAO_v4_400m.nc:z',
+            output_file='{output_dir}/bathymetric_chart.tif',
+            reproject_args=(
+                '-s_srs', '"+proj=stere +lat_0=90 +lat_ts=75 +datum=WGS84"',
+                '-dstnodata', '-9999',
+                '-tr', '400', '400',
+            ),
+            cut_file=project.boundaries['background'].filepath,
+        ),
+    ],
+)
