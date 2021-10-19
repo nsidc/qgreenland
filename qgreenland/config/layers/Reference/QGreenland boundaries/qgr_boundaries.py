03c738eafc321681b4b51b56ca4526a3b0e0d86c
Matt Fisher <matt.fisher@nsidc.org>
2021-10-19 14:20:30 -0600

    Fix tests

--- qgreenland/config/layers/Reference/QGreenland boundaries/qgr_boundaries.py
+++ qgreenland/config/layers/Reference/QGreenland boundaries/qgr_boundaries.py
@@ -1,7 +1,6 @@
 from qgreenland.config.datasets.boundaries import qgr_bounds as dataset
 from qgreenland.config.helpers.steps.ogr2ogr import ogr2ogr
 from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
-from qgreenland.models.config.step import ConfigLayerCommandStep
 
 
 qgr_boundary_data = ConfigLayer(
