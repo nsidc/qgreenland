2cd61f8ec2c088694c28dbdd7eac1ecb95de9ce2
Trey Stafford <trey.stafford@nsidc.org>
2021-10-20 11:53:06 -0600

    Migrate arctic dem layer

---
+++ qgreenland/config/helpers/steps/compress_raster.py
@@ -0,0 +1,16 @@
+from qgreenland.models.config.step import AnyStep, ConfigLayerCommandStep
+
+
+def compress_raster(
+        *,
+        input_file,
+        output_file,
+) -> list[AnyStep]:
+    return [ConfigLayerCommandStep(
+        args=[
+            'gdal_translate',
+            '-co', 'COMPRESS=DEFLATE',
+            input_file,
+            output_file,
+        ],
+    )]
