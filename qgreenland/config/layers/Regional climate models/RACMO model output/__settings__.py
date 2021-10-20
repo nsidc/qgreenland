6ff76c4aeaba68a8e34a41578d7059d71776abad
Matt Fisher <matt.fisher@nsidc.org>
2021-10-20 17:07:59 -0600

    Re-add wind vector/speed layers

--- qgreenland/config/layers/Regional climate models/RACMO model output/__settings__.py
+++ qgreenland/config/layers/Regional climate models/RACMO model output/__settings__.py
@@ -4,6 +4,8 @@ from qgreenland.models.config.layer_group import LayerGroupSettings
 
 settings = LayerGroupSettings(
     order=[
+        'layers.py:racmo_wind_vectors',
+        'layers.py:racmo_wind_speed',
         *[
             f'layers.py:{layer_id}'
             for layer_id in RACMO_LAYER_ORDER
