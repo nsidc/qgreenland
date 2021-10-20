6d21b5c166ac30a2749c4b9401d3d66b00111d86
Trey Stafford <trey.stafford@nsidc.org>
2021-10-20 17:18:28 -0600

    Add racmo wind vector layer ids `RACMO_LAYER_ORDER`

--- qgreenland/config/layers/Regional climate models/RACMO model output/__settings__.py
+++ qgreenland/config/layers/Regional climate models/RACMO model output/__settings__.py
@@ -4,8 +4,6 @@ from qgreenland.models.config.layer_group import LayerGroupSettings
 
 settings = LayerGroupSettings(
     order=[
-        'layers.py:racmo_wind_vectors',
-        'layers.py:racmo_wind_speed',
         *[
             f'layers.py:{layer_id}'
             for layer_id in RACMO_LAYER_ORDER
