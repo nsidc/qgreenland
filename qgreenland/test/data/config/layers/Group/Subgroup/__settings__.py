f387952385fe8314b5128e9fa0cc668df51de378
Matt Fisher <matt.fisher@nsidc.org>
2021-10-25 09:44:30 -0600

    Fix unit tests

--- qgreenland/test/data/config/layers/Group/Subgroup/__settings__.py
+++ qgreenland/test/data/config/layers/Group/Subgroup/__settings__.py
@@ -2,7 +2,7 @@ from qgreenland.models.config.layer_group import LayerGroupSettings
 
 settings = LayerGroupSettings(
     order=[
-        'examples.py:example_online',
-        'examples.py:example_raster',
+        ':example_online',
+        ':example_raster',
     ],
 )
