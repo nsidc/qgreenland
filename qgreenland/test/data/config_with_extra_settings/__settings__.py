494e295cd8627dcf787fab8ec764056bf80045b5
Matt Fisher <matt.fisher@nsidc.org>
2021-10-25 10:28:39 -0600

    Add new test data

---
+++ qgreenland/test/data/config_with_extra_settings/__settings__.py
@@ -0,0 +1,9 @@
+from qgreenland.models.config.layer_group import LayerGroupSettings
+
+settings = LayerGroupSettings(
+    order=[
+        'Group',
+        ':extra_layer',
+        'Extra group',
+    ],
+)
---
+++ qgreenland/test/data/config_with_missing_settings/__settings__.py
@@ -0,0 +1,7 @@
+from qgreenland.models.config.layer_group import LayerGroupSettings
+
+settings = LayerGroupSettings(
+    order=[
+        'Group',
+    ],
+)
