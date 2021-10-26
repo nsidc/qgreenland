5bc59cdd88c48998627b6bebd670904da74c471f
Trey Stafford <trey.stafford@nsidc.org>
2021-10-26 17:03:47 -0600

    Settings for "Geology" group

---
+++ qgreenland/config/layers/Geology/__settings__.py
@@ -0,0 +1,11 @@
+from qgreenland.models.config.layer_group import LayerGroupSettings
+
+
+settings = LayerGroupSettings(
+    order=[
+        ':earthquakes',
+        ':tectonic_plate_boundaries',
+        ':soil_types',
+        'Geological map',
+    ],
+)
