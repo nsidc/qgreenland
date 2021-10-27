0138f3ad2e3571af6700eaeb9e08a88ed3ea9f45
Matt Fisher <matt.fisher@nsidc.org>
2021-10-27 15:11:36 -0600

    Add "exclude manual assets" flag to run CLI

--- qgreenland/util/config/config.py
+++ qgreenland/util/config/config.py
@@ -14,11 +14,11 @@ THIS_DIR = Path(__file__).resolve().parent
 CONFIG_DIR = THIS_DIR.parent.parent / 'config'
 _CONFIG: Optional[Config] = None
 
-
 def init_config(
     *,
     include_patterns: tuple[str, ...] = (),
     exclude_patterns: tuple[str, ...] = (),
+    exclude_manual_assets: bool = False,
 ) -> None:
     global _CONFIG
 
@@ -30,6 +30,7 @@ def init_config(
         CONFIG_DIR.resolve(),
         include_patterns=include_patterns,
         exclude_patterns=exclude_patterns,
+        exclude_manual_assets=exclude_manual_assets,
     )
 
     if not _CONFIG.layers:
