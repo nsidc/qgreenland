13f1974c76af6647a0e40cc9cd59a2a57f332ac7
Matt Fisher <matt.fisher@nsidc.org>
2021-10-27 18:27:39 -0600

    Make `id` optional again
    Otherwise, the typechecker catches cases where it isn't passed.

--- qgreenland/models/config/step.py
+++ qgreenland/models/config/step.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from functools import cached_property
-from typing import Union
+from typing import Optional, Union
 
 from pydantic import root_validator
 
@@ -9,7 +9,7 @@ from qgreenland.util.runtime_vars import EvalStr
 
 
 class ConfigLayerStep(ABC):
-    id: str
+    id: Optional[str]
     type: str
 
     @abstractmethod
@@ -20,25 +20,26 @@ class ConfigLayerStep(ABC):
 class ConfigLayerCommandStep(QgrBaseModel, ConfigLayerStep):
     # TODO: Why do I have to re-specify `id` when it's already defined in the
     # ABC?
-    id: str
+    id: Optional[str]
     type: str = 'command'
 
     args: list[EvalStr]
 
     # Not sure why, but we can't get this to work with a regular validator.
-    @root_validator(pre=True)
+    @root_validator
     @classmethod
     def set_default_id(cls, values):
-        if 'id' not in values:
-            text = values['args'][0].lower()
+        if 'id' in values and values['id'] is not None:
+            return values
 
-            symbols = [' ', '-', '=', '\\', '.']
-            for symbol in symbols:
-                if symbol in text:
-                    text = text.replace(symbol, '_')
+        text = values['args'][0].lower()
 
-            values['id'] = text
+        symbols = [' ', '-', '=', '\\', '.']
+        for symbol in symbols:
+            if symbol in text:
+                text = text.replace(symbol, '_')
 
+        values['id'] = text
         return values
 
     @cached_property
