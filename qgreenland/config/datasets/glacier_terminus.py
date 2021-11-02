67cc3a79bdcea827f06d61eb3ae25164ff80cc73
Trey Stafford <trey.stafford@nsidc.org>
2021-11-02 14:52:38 -0600

    Update glacier terminus dataset to v2

--- qgreenland/config/datasets/glacier_terminus.py
+++ qgreenland/config/datasets/glacier_terminus.py
@@ -7,73 +7,96 @@ glacier_terminus = ConfigDataset(
     assets=[
         ConfigDatasetCmrAsset(
             id='glacier_ids',
-            granule_ur='SC:NSIDC-0642.001:125854253',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146975',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2000_2001',
-            granule_ur='SC:NSIDC-0642.001:125860172',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146979',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2005_2006',
-            granule_ur='SC:NSIDC-0642.001:125860185',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146973',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2006_2007',
-            granule_ur='SC:NSIDC-0642.001:125860176',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146977',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2007_2008',
-            granule_ur='SC:NSIDC-0642.001:125860183',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146974',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2008_2009',
-            granule_ur='SC:NSIDC-0642.001:125860171',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146982',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2012_2013',
-            granule_ur='SC:NSIDC-0642.001:125860173',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146972',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2014_2015',
-            granule_ur='SC:NSIDC-0642.001:125860177',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146970',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2015_2016',
-            granule_ur='SC:NSIDC-0642.001:125860175',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146978',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
         ConfigDatasetCmrAsset(
             id='2016_2017',
-            granule_ur='SC:NSIDC-0642.001:125860184',
-            collection_concept_id='C1413880084-NSIDC_ECS',
+            granule_ur='SC:NSIDC-0642.002:227146971',
+            collection_concept_id='C2139015179-NSIDC_ECS',
+        ),
+        ConfigDatasetCmrAsset(
+            id='2017_2018',
+            granule_ur='SC:NSIDC-0642.002:227146980',
+            collection_concept_id='C2139015179-NSIDC_ECS',
+        ),
+        ConfigDatasetCmrAsset(
+            id='2018_2019',
+            granule_ur='SC:NSIDC-0642.002:227146976',
+            collection_concept_id='C2139015179-NSIDC_ECS',
+        ),
+        ConfigDatasetCmrAsset(
+            id='2019_2020',
+            granule_ur='SC:NSIDC-0642.002:227146981',
+            collection_concept_id='C2139015179-NSIDC_ECS',
+        ),
+        ConfigDatasetCmrAsset(
+            id='2020_2021',
+            granule_ur='SC:NSIDC-0642.002:227146983',
+            collection_concept_id='C2139015179-NSIDC_ECS',
         ),
     ],
     metadata={
-        'title': 'MEaSUREs Annual Greenland Outlet Glacier Terminus Positions from SAR Mosaics, Version 1',
+        'title': 'MEaSUREs Annual Greenland Outlet Glacier Terminus Positions from SAR Mosaics, Version 2',
         'abstract': (
             """This data set, part of the NASA Making Earth System Data Records
             for Use in Research Environments (MEaSUREs) program, provides
-            Greenland outlet glacier terminus positions created from MEaSUREs
-            Synthetic Aperture Radar (SAR) mosaics and Landsat 8 OLI imagery."""
+            digitized terminus positions for 239 Greenland outlet glaciers
+            during 13 winter seasons. Terminus positions were determined from
+            MEaSUREs mosaics of Sentinel-1 and RADARSAT-1 synthetic aperture
+            radar data, with gaps filled using Landsat-7 and -8 panchromatic
+            imagery."""
         ),
         'citation': {
             'text': (
-                """Joughin, I., T. Moon, J. Joughin, and T. Black. 2015, 2017.
-                MEaSUREs Annual Greenland Outlet Glacier Terminus Positions from
-                SAR Mosaics, Version 1. [Indicate subset used]. Boulder,
-                Colorado USA. NASA National Snow and Ice Data Center Distributed
-                Active Archive Center. doi:
-                https://doi.org/10.5067/DC0MLBOCL3EL. 2020/02/12."""
+                """Joughin, I., T. Moon, J. Joughin, and
+                T. Black. 2021. MEaSUREs Annual Greenland Outlet Glacier
+                Terminus Positions from SAR Mosaics, Version 2. [Indicate subset
+                used]. Boulder, Colorado USA. NASA National Snow and Ice Data
+                Center Distributed Active Archive Center. doi:
+                https://doi.org/10.5067/ESFWE11AVFKW. 2021/11/02."""
             ),
-            'url': 'https://doi.org/10.5067/DC0MLBOCL3EL',
+            'url': 'https://doi.org/10.5067/ESFWE11AVFKW',
         },
     },
 )
