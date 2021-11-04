3a3846b423eeee2522916f38d37df1663d9c11ae
Matt Fisher <matt.fisher@nsidc.org>
2021-11-04 11:38:43 -0600

    Fix rsync script

--- scripts/nsidc/push_release_to_prod.sh
+++ scripts/nsidc/push_release_to_prod.sh
@@ -7,8 +7,9 @@ PROD_HOSTNAME='qgreenland.apps.int.nsidc.org'
 VAGRANT_SSH_KEY="$HOME/.ssh/id_rsa_vagrant_vsphere"
 
 echo "WARNING: THIS WILL OVERWRITE PRODUCTION LAYERS WITH LOCAL LAYERS AND SYNC ALL OF YOUR RELEASES TO PRODUCTION."
-read -p "PRESS CTRL-C TO CANCEL. PRESS ENTER TO PUSH STRAIGHT TO PROD AND DESTROY EVERYTHING."
+read -rp "PRESS CTRL-C TO CANCEL. PRESS ENTER TO PUSH STRAIGHT TO PROD AND DESTROY EVERYTHING."
 
-rsync -a --progress --verbose \
+sudo rsync --archive --progress --verbose \
     -e "ssh -i ${VAGRANT_SSH_KEY}" \
+    --rsync-path='sudo rsync' \
     ${LOCAL_RELEASE_DIR}/* vagrant@${PROD_HOSTNAME}:${PROD_RELEASE_DIR}/
