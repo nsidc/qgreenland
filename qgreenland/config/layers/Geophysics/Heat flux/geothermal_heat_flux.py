91fd856fa6f5118bdee77e92e6a09103ea660650
Trey Stafford <trey.stafford@nsidc.org>
2021-11-10 11:00:51 -0700

    Fixup layer steps for geothermal heat flux
    The new URL for this layer downloads a .nc file instead of a .zip.

--- qgreenland/config/layers/Geophysics/Heat flux/geothermal_heat_flux.py
+++ qgreenland/config/layers/Geophysics/Heat flux/geothermal_heat_flux.py
@@ -8,10 +8,6 @@ from qgreenland.config.project import project
 from qgreenland.models.config.layer import ConfigLayer, ConfigLayerInput
 
 
-extract_fp = (
-    'ADS/ArCS-T2/A20180227-001/v200/dataset/'
-    'GHF_Greenland_Ver2.0_GridEPSG3413_05km.nc'
-)
 nc_dataset = 'GHF'
 
 geothermal_heat_flux = ConfigLayer(
@@ -28,12 +24,8 @@ geothermal_heat_flux = ConfigLayer(
         asset=dataset.assets['only'],
     ),
     steps=[
-        decompress_step(
-            input_file='{input_dir}/A20180227-001v200.zip',
-            decompress_contents_mask=extract_fp,
-        ),
         *warp(
-            input_file='NETCDF:{input_dir}/' + f'{extract_fp}:{nc_dataset}',
+            input_file='NETCDF:{input_dir}/' + f'GHF_Greenland_Ver2.0_GridEPSG3413_05km.nc:{nc_dataset}',
             output_file='{output_dir}/warped.tif',
             cut_file=project.boundaries['data'].filepath,
         ),
