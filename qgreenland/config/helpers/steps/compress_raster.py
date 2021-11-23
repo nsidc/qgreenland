5e085064b5ca0ef847c6508e6ab553a2e5cc9da6
Trey Stafford <trey.stafford@nsidc.org>
2021-11-22 17:25:43 -0700

    WIP add arg to `compress_raster` for determining predictor value

--- qgreenland/config/helpers/steps/compress_raster.py
+++ qgreenland/config/helpers/steps/compress_raster.py
@@ -3,13 +3,18 @@ from qgreenland.models.config.step import AnyStep, CommandStep
 
 def compress_raster(
     *,
-    input_file,
-    output_file,
+    input_file: str,
+    output_file: str,
+    dtype_is_float: bool,
 ) -> list[AnyStep]:
+    predictor_value = 3 if dtype_is_float else 2
+
     return [CommandStep(
         args=[
             'gdal_translate',
             '-co', 'COMPRESS=DEFLATE',
+            '-co', f'PREDICTOR={predictor_value}',
+            # TODO: `-co TILED=yes`
             input_file,
             output_file,
         ],
