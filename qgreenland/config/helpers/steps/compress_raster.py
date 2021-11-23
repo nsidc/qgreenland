8baa9001611315479f32c29b584156ab6304270e
Matt Fisher <matt.fisher@nsidc.org>
2021-11-22 17:55:56 -0700

    Revert more!

--- qgreenland/config/helpers/steps/compress_raster.py
+++ qgreenland/config/helpers/steps/compress_raster.py
@@ -7,13 +7,10 @@ def compress_raster(
     input_file: str,
     output_file: str,
 ) -> list[AnyStep]:
-    predictor_value = 3 if dtype_is_float else 2
-
     return [CommandStep(
         args=[
             'gdal_translate',
             '-co', 'COMPRESS=DEFLATE',
-            '-co', f'PREDICTOR={predictor_value}',
             # TODO: `-co TILED=yes`
             input_file,
             output_file,
