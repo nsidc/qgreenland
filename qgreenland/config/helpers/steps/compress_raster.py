4356c895897938991e9f4ded9b7b474026e8478c
Matt Fisher <matt.fisher@nsidc.org>
2021-11-22 17:47:56 -0700

    Add notes to delete OBE step helpers

--- qgreenland/config/helpers/steps/compress_raster.py
+++ qgreenland/config/helpers/steps/compress_raster.py
@@ -1,3 +1,4 @@
+# TODO: DELETE ME
 from qgreenland.models.config.step import AnyStep, CommandStep
 
 
@@ -9,13 +10,19 @@ def compress_raster(
 ) -> list[AnyStep]:
     predictor_value = 3 if dtype_is_float else 2
 
-    return [CommandStep(
-        args=[
-            'gdal_translate',
-            '-co', 'COMPRESS=DEFLATE',
-            '-co', f'PREDICTOR={predictor_value}',
-            # TODO: `-co TILED=yes`
-            input_file,
-            output_file,
-        ],
-    )]
+    return [
+        CommandStep(
+            args=[
+                'gdal_translate',
+                '-co', 'COMPRESS=DEFLATE',
+                '-co', f'PREDICTOR={predictor_value}',
+                # TODO: `-co TILED=yes`
+                input_file,
+                '{output_dir}/compressed.tif',
+            ],
+        ),
+        *build_overviews(
+            input_file=
+            output_file=output_file,
+        )
+    ]
