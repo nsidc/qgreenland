68c20c0bbf36534d2f595d1f96c5a21847a1a768
Matt Fisher <matt.fisher@nsidc.org>
2021-11-23 11:58:35 -0700

    Add provenance CLI utility

--- qgreenland/cli/__init__.py
+++ qgreenland/cli/__init__.py
@@ -3,6 +3,7 @@ import click
 from qgreenland.cli.cleanup import cleanup
 from qgreenland.cli.config_template import config_template
 from qgreenland.cli.fetch import fetch
+from qgreenland.cli.provenance import provenance
 from qgreenland.cli.run import run
 
 
@@ -15,6 +16,7 @@ cli.add_command(cleanup)
 cli.add_command(config_template)
 cli.add_command(run)
 cli.add_command(fetch)
+cli.add_command(provenance)
 
 
 if __name__ == '__main__':
