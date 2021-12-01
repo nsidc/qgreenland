bebff203e3be96b6829c5b8666225ea95e891537
Trey Stafford <trey.stafford@nsidc.org>
2021-11-30 17:53:27 -0700

    Cleanup JPEG compression artifacts at edges with mask band
    This looks pretty complicated but it works! TODO: add comments to explain each
    step.

--- qgreenland/config/layers/Basemaps/background.py
+++ qgreenland/config/layers/Basemaps/background.py
@@ -3,6 +3,16 @@ from qgreenland.config.helpers.steps.compress_and_add_overviews import compress_
 from qgreenland.config.helpers.steps.decompress import decompress_step
 from qgreenland.config.helpers.steps.warp_and_cut import warp_and_cut
 from qgreenland.models.config.layer import Layer, LayerInput
+from qgreenland.models.config.step import CommandStep
+
+
+def _separate_band(idx) -> list[str]:
+    return [
+        'gdal_translate',
+        '-b', str(idx),
+        '{input_dir}/input.tif',
+        '{output_dir}/' + f'b{idx}.tif'
+    ]
 
 
 background = Layer(
@@ -32,13 +42,54 @@ background = Layer(
             ],
             cut_file='{assets_dir}/latitude_shape_40_degrees.geojson',
         ),
+        CommandStep(
+            id='create_mask',
+            args=[
+                'cp', '{input_dir}/warped_and_cut.tif', '{output_dir}/input.tif',
+                '&&',
+                'gdal_calc.py',
+                '--calc="numpy.invert(numpy.isnan(A)).astype(int)"',
+                '--outfile={output_dir}/mask.tif',
+                '-A', '{output_dir}/input.tif',
+                '--A_band=1',
+            ],
+        ),
+        CommandStep(
+            id='separate_bands',
+            args=[
+                'cp', '{input_dir}/*', '{output_dir}/',
+                '&&',
+                *_separate_band(1),
+                '&&',
+                *_separate_band(2),
+                '&&',
+                *_separate_band(3),
+            ],
+        ),
+        # TODO: consider DEFLATE compression for this step. Output is about 2
+        # GB.
+        CommandStep(
+            id='merge_bands',
+            args=[
+                'gdal_merge.py',
+                '-o', '{output_dir}/merged.tif',
+                '-separate',
+                '{input_dir}/b1.tif',
+                '{input_dir}/b2.tif',
+                '{input_dir}/b3.tif',
+                '{input_dir}/mask.tif',
+            ],
+        ),
         *compress_and_add_overviews(
-            input_file='{input_dir}/warped_and_cut.tif',
+            input_file='{input_dir}/merged.tif',
             output_file='{output_dir}/overviews.tif',
             compress_type='JPEG',
             compress_args=[
                 '-co', 'JPEG_QUALITY=90',
                 '-co', 'PHOTOMETRIC=YCBCR',
+                '-b', '1', '-b', '2', '-b', '3',
+                '-mask', '4',
+                '--config', 'GDAL_TIFF_INTERNAL_MASK', 'YES',
             ],
         ),
     ],
