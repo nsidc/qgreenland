7faf8f654a765a159b162728c141cebe65549c60
Trey Stafford <trey.stafford@nsidc.org>
2021-12-01 14:54:31 -0700

    Remove sea ice wfs (time controller) layer
    The default time-controller settings are not very usable. We will be creating
    documentation on how to add time controller layers manually

--- qgreenland/config/layers/Internet-required data/online.py
+++ qgreenland/config/layers/Internet-required data/online.py
@@ -1,7 +1,4 @@
-from qgreenland.config.datasets.online import (
-    image_mosaic,
-    sea_ice_index,
-)
+from qgreenland.config.datasets.online import image_mosaic
 from qgreenland.models.config.layer import Layer, LayerInput
 
 
@@ -20,18 +17,3 @@ image_mosaic_layers = [
 
     for year, resolution in (('2015', '15'), ('2019', '10'))
 ]
-
-
-g02135_polyline_n = Layer(
-    id='g02135_polyline_n',
-    title='Monthly sea ice extent (1979 - present)',
-    style='g02135_polyline_n',
-    description="""
-Lines representing monthly sea ice extent edges. Note that aside from 2016 to
-present, most of the data is missing.""",
-    tags=['online'],
-    input=LayerInput(
-        dataset=sea_ice_index,
-        asset=sea_ice_index.assets['monthly_polyline_n'],
-    ),
-)
