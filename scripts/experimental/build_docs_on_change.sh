94880596cb8007639978f729cb25dc7b7f3ba600
Matt Fisher <matt.fisher@nsidc.org>
2021-12-07 12:57:27 -0700

    Shellcheck :bell:

--- scripts/experimental/build_docs_on_change.sh
+++ scripts/experimental/build_docs_on_change.sh
@@ -2,9 +2,9 @@
 set -e
 
 THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
-cd $THIS_DIR/../../doc
+cd "$THIS_DIR"/../../doc
 
 make clean && make html
-while inotifywait -e modify -e delete -e create -e close_write -r *; do
+while inotifywait -e modify -e delete -e create -e close_write -r ./; do
     make clean && make html;
 done
