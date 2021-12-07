9ebbceed863542ac0d3cc2ad13804cd6942d3aa7
Matt Fisher <matt.fisher@nsidc.org>
2021-12-07 11:53:03 -0700

    Make the build-on-change script react to more things
    -r: Recursive
    -e: Support more events

--- scripts/experimental/build_docs_on_change.sh
+++ scripts/experimental/build_docs_on_change.sh
@@ -5,6 +5,6 @@ THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
 cd $THIS_DIR/../../doc
 
 make clean && make html
-while inotifywait -e close_write *; do
+while inotifywait -e modify -e delete -e create -e close_write -r *; do
     make clean && make html;
 done
