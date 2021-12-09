8b4720ea3fd41a7a9e95c341f6026a8c7dd08ec4
Hope Simonoko <hope.simonoko@colorado.edu>
2021-12-09 12:02:58 -0700

    add helper

---
+++ qgreenland/config/helpers/layers/continental_shelf.py
@@ -0,0 +1,91 @@
+from qgreenland.config.datasets.continental_shelf import continental_shelf as dataset
+from qgreenland.config.helpers.steps.compressed_vector import compressed_vector
+from qgreenland.models.config.layer import Layer, LayerInput
+
+
+LAYER_PARAMS = {
+    'north_points': {
+        'title': 'Continental shelf points (north)',
+        'description': (
+            """Northern continental shelf of Greenland."""
+        ),
+        'asset_id': 'north_points',
+    },
+    'north_lines': {
+        'title': 'Continental shelf lines (north)',
+        'description': (
+            """Northern continental shelf of Greenland."""
+        ),
+        'asset_id': 'north_lines',
+    },
+    'north_polygons': {
+        'title': 'Continental shelf poloygons (north)',
+        'description': (
+            """Northern continental shelf of Greenland."""
+        ),
+        'asset_id': 'north_polygons',
+    },
+    'northeast_points': {
+        'title': 'Continental shelf (northeast)',
+        'description': (
+            """Northeastern continental shelf of Greenland."""
+        ),
+        'asset_id': 'northeast_points',
+    },
+    'northeast_lines': {
+        'title': 'Continental shelf (northeast)',
+        'description': (
+            """Northeastern continental shelf of Greenland."""
+        ),
+        'asset_id': 'northeast_lines',
+    },
+    'northeast_polygons': {
+        'title': 'Continental shelf (northeast)',
+        'description': (
+            """Northeastern continental shelf of Greenland."""
+        ),
+        'asset_id': 'northeast_polygons',
+    },
+    'south_points': {
+        'title': 'Continental shelf (south)',
+        'description': (
+            """Southern continental shelf of Greenland."""
+        ),
+        'asset_id': 'south_points',
+    },
+    'south_lines': {
+        'title': 'Continental shelf (south)',
+        'description': (
+            """Southern continental shelf of Greenland."""
+        ),
+        'asset_id': 'south_lines',
+    },
+    'south_polygons': {
+        'title': 'Continental shelf (south)',
+        'description': (
+            """Southern continental shelf of Greenland."""
+        ),
+        'asset_id': 'south_polygons',
+    },
+}
+
+
+def make_layers() -> list[Layer]:
+    return [
+        Layer(
+            id=key,
+            title=params['title'],
+            description=params['description'],
+            tags=[],
+            input=LayerInput(
+                dataset=dataset,
+                asset=dataset.assets[params['asset_id']],
+            ),
+            steps=[
+                *compressed_vector(
+                    input_file='{input_dir}/*.shp',
+                    output_file='{output_dir}/final.gpkg',
+                ),
+            ],
+        ) for key, params in LAYER_PARAMS.items()
+    ]
--- qgreenland/config/layers/Environmental management/Maritime boundaries/__settings__.py
+++ qgreenland/config/layers/Environmental management/Maritime boundaries/__settings__.py
@@ -1,9 +1,16 @@
-from qgreenland.config.helpers.layers.territorial_waters import LAYER_PARAMS
+from qgreenland.config.helpers.layers.territorial_waters import (
+    LAYER_PARAMS as territorial_waters_layers,
+)
+from qgreenland.config.helpers.layers.continental_shelf import (
+    LAYER_PARAMS as continental_shelf_layers,
+)
 from qgreenland.models.config.layer_group import LayerGroupSettings
 
 
 settings = LayerGroupSettings(
     order=[
-        f':{layer_id}' for layer_id in LAYER_PARAMS.keys()
+        f':{layer_id}' for
+        layer_id in territorial_waters_layers.keys() for
+        asset_id in continental_shelf_layers.keys()
     ],
 )
