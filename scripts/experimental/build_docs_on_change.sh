d3619915eaffc6dcb3f6816ad8a19358671a3436
Matt Fisher <matt.fisher@nsidc.org>
2021-12-09 10:03:19 -0700

    Cleanup doc-build script
    Also, open the browser!

--- scripts/experimental/build_docs_on_change.sh
+++ scripts/experimental/build_docs_on_change.sh
@@ -3,8 +3,19 @@ set -e
 
 THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
 cd "$THIS_DIR"/../../doc
+HTML_PATH="./_build/html/index.html"
 
-make clean && make html || true
+make clean
+
+set +e
+make html
+xdg-open "${HTML_PATH}"
+set -e
+
 while inotifywait -e delete -e create -e close_write -r ./; do
-    make clean && make html || true;
+    make clean
+
+    set +e
+    make html
+    set -e
 done
