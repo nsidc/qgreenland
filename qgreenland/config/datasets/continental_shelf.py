d158f63edbb60e1a022ccebc807110a326b9e4f2
Hope Simonoko <hope.simonoko@colorado.edu>
2021-12-10 16:07:30 -0700

    add abstract and citation

--- qgreenland/config/datasets/continental_shelf.py
+++ qgreenland/config/datasets/continental_shelf.py
@@ -63,13 +63,26 @@ continental_shelf = Dataset(
     metadata={
         'title': 'Denmark - in respect of the Continental Shelf of Greenland',
         'abstract': (
-            """TODO: ."""
+            """On 15 December 2014, the Kingdom of Denmark submitted to the
+            Commission on the Limits of the Continental Shelf, in accordance with Article 76, paragraph 8,
+            of the Convention, information on the limits of the continental shelf beyond 200 nautical miles
+            from the baselines from which the breadth of its territorial sea is measured in respect of
+            the Northern Continental Shelf of Greenland. It is noted that the Convention entered into force
+            for Denmark on 16 December 2004. According to the submitting State, this is a “partial submission
+            of the Government of the Kingdom of Denmark together with the Government of Greenland”.
+            In accordance with the Rules of Procedure of the Commission (CLCS/40/Rev.1), a communication is being
+            circulated to all Member States of the United Nations, as well as States Parties to the Convention,
+            in order to make public the executive summary of the partial submission, including all charts and
+            coordinates contained in that summary. The consideration of the submission made by Denmark will be
+            included in the provisional agenda of the thirty-eighth session of the Commission to be held in
+            New York from 20 July to 4 September 2015. Upon completion of the consideration of the submission,
+            the Commission shall make recommendations pursuant to Article 76 of the Convention."""
         ),
         'citation': {
             'text': (
-                """TODO: ."""
+                """Kingdom of Denmark together with the Government of Greenland."""
             ),
-            'url': 'http://example.com',
+            'url': 'http://continentalshelf.org/onestopdatashop/6350.aspx',
         },
     },
 )
