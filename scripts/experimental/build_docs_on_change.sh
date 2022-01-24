8759f8b28fe52e4d07417ed9d9133e772df8ab0f
Matt Fisher <matt.fisher@nsidc.org>
2022-01-24 14:41:38 -0700

    Update build_docs_on_change script to work on OSX (I think)

--- scripts/experimental/build_docs_on_change.sh
+++ scripts/experimental/build_docs_on_change.sh
@@ -3,6 +3,14 @@
 
 set -e
 
+if [[ $OSTYPE == 'darwin'* ]]; then
+    open_cmd='open'
+    inotify_cmd='fswatch ./'
+else
+    open_cmd='xdg-open'
+    inotify_cmd='inotifywait -e delete -e create -e close_write -r ./'
+fi
+
 THIS_DIR="$( cd "$(dirname "$0")"; pwd -P )"
 cd "$THIS_DIR"/../../doc
 HTML_PATH="./_build/html/index.html"
@@ -11,10 +19,10 @@ make clean
 
 set +e
 make html
-xdg-open "${HTML_PATH}"
+$open_cmd "${HTML_PATH}"
 set -e
 
-while inotifywait -e delete -e create -e close_write -r ./; do
+while $inotify_cmd; do
     make clean
 
     set +e
