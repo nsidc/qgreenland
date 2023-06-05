9a26cfa4e57522150e46fd5c1ca1934fdfcd3fe4
Matt Fisher <matt.fisher@nsidc.org>
2023-06-05 15:15:46 -0600

    Run ruff, black, vulture, shellcheck, and more with pre-commit

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,7 +1,6 @@
 from invoke import Collection
 
 from . import config, docs, env
-from . import format as _format
 from . import test
 
 ns = Collection()
@@ -9,4 +8,3 @@ ns.add_collection(config)
 ns.add_collection(docs)
 ns.add_collection(env)
 ns.add_collection(test)
-ns.add_collection(_format)
