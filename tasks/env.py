8c1a187ea3490c9fc2b1999738a6085a8d21a472
Matt Fisher <matt.fisher@nsidc.org>
2023-06-08 17:24:06 -0600

    Use `conda-lock` instead of `conda env export` for locking environment

--- tasks/env.py
+++ tasks/env.py
@@ -2,31 +2,10 @@ from invoke import task
 
 from .util import PROJECT_DIR, print_and_run
 
-ENV_LOCKFILE = PROJECT_DIR / "environment-lock.yml"
+ENV_LOCKFILE = PROJECT_DIR / "environment.yml"
 
 
 @task(default=True)
 def lock(ctx):
-    """Update the environment-lock.yml file from the current `qgreenland` environment."""
-    print_and_run(f"conda env export -n qgreenland > {ENV_LOCKFILE}")
-
-    with open(ENV_LOCKFILE, "r") as f:
-        lines = f.readlines()
-
-    with open(ENV_LOCKFILE, "w") as f:
-        for line in lines:
-            # The prefix line contains machine-specific directory
-            if line.startswith("prefix: "):
-                continue
-
-            # We don't want to use the NSIDC conda channel
-            if "- nsidc" in line:
-                continue
-
-            # We want to replace the "defaults" channel with "nodefaults" so conda-forge
-            # is used for everything.
-            if "- defaults" in line:
-                f.write(line.replace("- defaults", "- nodefaults"))
-                continue
-
-            f.write(line)
+    """Update the `conda-lock.yml` file from the `environment.yml` spec."""
+    print_and_run(f"conda-lock -f {ENV_LOCKFILE} -p linux-64")
