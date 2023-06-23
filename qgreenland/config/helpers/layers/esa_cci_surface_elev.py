a7fcaf36b9c02179b4b9cd5365d1ecf6d522130d
Trey Stafford <trey.stafford@colorado.edu>
2023-06-23 14:11:37 -0600

    Update the ESA CCI Surface Elevation Change layers

--- qgreenland/config/helpers/layers/esa_cci_surface_elev.py
+++ qgreenland/config/helpers/layers/esa_cci_surface_elev.py
@@ -6,12 +6,13 @@ from qgreenland.config.datasets.esa_cci import (
 from qgreenland.config.helpers.steps.compress_and_add_overviews import (
     compress_and_add_overviews,
 )
+from qgreenland.config.helpers.steps.decompress import decompress_step
 from qgreenland.models.config.layer import Layer, LayerInput
 from qgreenland.models.config.step import CommandStep
 
 _year_ranges = [
     (start_year, end_year)
-    for start_year, end_year in zip(range(1992, 2015 + 1), range(1996, 2019 + 1))
+    for start_year, end_year in zip(range(1992, 2016 + 1), range(1996, 2020 + 1))
 ]
 
 
@@ -26,10 +27,16 @@ def surface_elevation_layer(
     variable: SurfaceElevVar,
 ) -> Layer:
     if variable == "SEC":
-        description = "Rate of surface elevation change in meters per year."
+        description = """Rate of surface elevation change in meters per year.
+
+               Data are based on ESAs Ku-band radar satellite level-2 data
+               products."""
         style = "surface_elevation_change"
     else:
-        description = "Error of rate of surface elevation change in meters per year."
+        description = """Error of rate of surface elevation change in meters per year.
+
+            Data are based on ESAs Ku-band radar satellite level-2 data
+            products."""
         style = "surface_elevation_change_errors"
 
     return Layer(
@@ -43,6 +50,9 @@ def surface_elevation_layer(
             asset=dataset.assets["only"],
         ),
         steps=[
+            decompress_step(
+                input_file="{input_dir}/cci_sec_2021.zip",
+            ),
             CommandStep(
                 args=[
                     "gdalmdimtranslate",
@@ -50,7 +60,7 @@ def surface_elevation_layer(
                     "COMPRESS=DEFLATE",
                     "-array",
                     f"name={variable},view=[:,:,{array_index}]",
-                    "{input_dir}/Release/CCI_GrIS_RA_SEC_5km_Vers2.0_2020-08-26.nc",
+                    "{input_dir}/Release/CCI_GrIS_RA_SEC_5km_Vers3.0_2021-08-09.nc",
                     "{output_dir}/" + f"{variable.lower()}_{start_year}_{end_year}.tif",
                 ],
             ),
--- scripts/private-archive-preprocess/esa_cci_surface_elevation_change/preprocess.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /share/appdata/qgreenland-private-archive/esa_cci_surface_elevation_change
-tar -xvf cci_sec_2020.tar.gz
