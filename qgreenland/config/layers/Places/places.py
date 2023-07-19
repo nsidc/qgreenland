c58e48f2f26e804dd263a2103a3ade712f97875e
Trey Stafford <trey.stafford@colorado.edu>
2023-07-19 13:22:32 -0600

    DRY out layer config for cities and settlements

--- qgreenland/config/layers/Places/places.py
+++ qgreenland/config/layers/Places/places.py
@@ -3,56 +3,32 @@ from qgreenland.config.helpers.steps.ogr2ogr import ogr2ogr
 from qgreenland.config.project import project
 from qgreenland.models.config.layer import Layer, LayerInput
 
-cities = Layer(
-    id="cities",
-    title="Cities",
-    style="labeled_point",
-    description="""Lines representing roads in Greenland.""",
-    tags=["places"],
-    input=LayerInput(
-        dataset=asiaq_nunagis,
-        asset=asiaq_nunagis.assets["cities"],
-    ),
-    steps=[
-        *ogr2ogr(
-            input_file="{input_dir}/fetched.geojson",
-            output_file="{output_dir}/final.gpkg",
-            boundary_filepath=project.boundaries["data"].filepath,
-            ogr2ogr_args=(
-                "-nln",
-                "cities",
-                "-dialect",
-                "sqlite",
-                "-sql",
-                "\"SELECT geometry, Ny_grønlandsk as 'New Greenlandic', Ny_grønlandsk as 'label', Gammel_grønlandsk as 'Old Greenlandic', Dansk as Danish, Alternativt_stednavn  as 'Alternative placename', Indbyggertal_2016 as 'Population 2016' FROM ESRIJSON\"",
-            ),
+layers = [
+    Layer(
+        id=layer_id,
+        title=layer_id.capitalize(),
+        style="labeled_point",
+        description=f"Points representing {layer_id} in Greenland.",
+        tags=["places"],
+        input=LayerInput(
+            dataset=asiaq_nunagis,
+            asset=asiaq_nunagis.assets[layer_id],
         ),
-    ],
-)
-
-settlements = Layer(
-    id="settlements",
-    title="Settlements",
-    style="labeled_point",
-    description="""Lines representing roads in Greenland.""",
-    tags=["places"],
-    input=LayerInput(
-        dataset=asiaq_nunagis,
-        asset=asiaq_nunagis.assets["settlements"],
-    ),
-    steps=[
-        *ogr2ogr(
-            input_file="{input_dir}/fetched.geojson",
-            output_file="{output_dir}/final.gpkg",
-            boundary_filepath=project.boundaries["data"].filepath,
-            ogr2ogr_args=(
-                "-nln",
-                "settlements",
-                "-dialect",
-                "sqlite",
-                "-sql",
-                "\"SELECT geometry, Ny_grønlandsk as 'New Greenlandic', Ny_grønlandsk as 'label', Gammel_grønlandsk as 'Old Greenlandic', Dansk as Danish, Alternativt_stednavn  as 'Alternative placename', Indbyggertal_2016 as 'Population 2016' FROM ESRIJSON\"",
+        steps=[
+            *ogr2ogr(
+                input_file="{input_dir}/fetched.geojson",
+                output_file="{output_dir}/final.gpkg",
+                boundary_filepath=project.boundaries["data"].filepath,
+                ogr2ogr_args=(
+                    "-nln",
+                    layer_id,
+                    "-dialect",
+                    "sqlite",
+                    "-sql",
+                    "\"SELECT geometry, Ny_grønlandsk as 'New Greenlandic', Ny_grønlandsk as 'label', Gammel_grønlandsk as 'Old Greenlandic', Dansk as Danish, Alternativt_stednavn  as 'Alternative placename', Indbyggertal_2016 as 'Population 2016' FROM ESRIJSON\"",
+                ),
             ),
-        ),
-    ],
-)
+        ],
+    )
+    for layer_id in ("cities", "settlements")
+]
