a78a3893eecccf78432e779bff25fb99d230c7b8
Trey Stafford <trey.stafford@colorado.edu>
2023-07-19 14:55:11 -0600

    Move comprehensive place names layer to bottom of Places layer group

--- qgreenland/config/layers/Places/__settings__.py
+++ qgreenland/config/layers/Places/__settings__.py
@@ -2,10 +2,10 @@ from qgreenland.models.config.layer_group import LayerGroupSettings
 
 settings = LayerGroupSettings(
     order=[
-        ":comprehensive_places",
         ":buildings",
         ":roads",
         ":cities",
         ":settlements",
+        ":comprehensive_places",
     ],
 )
