18abe0e3ac45a1b64e6554a61ff64020b5abb7bf
Trey Stafford <trey.stafford@colorado.edu>
2023-07-19 15:10:11 -0600

    Reorder Places layer group

--- qgreenland/config/layers/Places/__settings__.py
+++ qgreenland/config/layers/Places/__settings__.py
@@ -2,10 +2,10 @@ from qgreenland.models.config.layer_group import LayerGroupSettings
 
 settings = LayerGroupSettings(
     order=[
-        ":buildings",
-        ":roads",
         ":cities",
         ":settlements",
         ":comprehensive_places",
+        ":roads",
+        ":buildings",
     ],
 )
