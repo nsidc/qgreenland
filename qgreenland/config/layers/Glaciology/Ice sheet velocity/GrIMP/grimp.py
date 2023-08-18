43b977dc4fb6b9a0743ce42aafccb7b0bc96bed1
Trey Stafford <trey.stafford@colorado.edu>
2023-08-18 12:22:58 -0600

    WIP attempt scaling annual velocity mosaics

--- qgreenland/config/layers/Glaciology/Ice sheet velocity/GrIMP/grimp.py
+++ qgreenland/config/layers/Glaciology/Ice sheet velocity/GrIMP/grimp.py
@@ -2,7 +2,9 @@ from qgreenland.config.datasets.grimp import grimp_annual_ice_velocity as datase
 from qgreenland.config.helpers.steps.compress_and_add_overviews import (
     compress_and_add_overviews,
 )
+from qgreenland.config.helpers.steps.gdal_edit import gdal_edit
 from qgreenland.models.config.layer import Layer, LayerInput
+from qgreenland.models.config.step import CommandStep
 
 grimp_vv_veloity_layers = [
     Layer(
@@ -17,10 +19,33 @@ grimp_vv_veloity_layers = [
             asset=dataset.assets[layer_id],
         ),
         steps=[
+            CommandStep(
+                args=[
+                    "gdal_calc.py",
+                    "--calc",
+                    '"round(A * 100.0)"',
+                    "--NoDataValue",
+                    "-9999",
+                    "--type",
+                    "Int32",
+                    "-A",
+                    "{input_dir}/*_vv_*.tif",
+                    "--outfile",
+                    "{output_dir}/scaled.tif",
+                ],
+            ),
+            *gdal_edit(
+                input_file="{input_dir}/scaled.tif",
+                output_file="{output_dir}/edited.tif",
+                gdal_edit_args=[
+                    "-scale",
+                    "0.01",
+                ],
+            ),
             *compress_and_add_overviews(
-                input_file="{input_dir}/*_vv_*.tif",
-                output_file="{output_dir}/vv.tif",
-                dtype_is_float=True,
+                input_file="{input_dir}/edited.tif",
+                output_file="{output_dir}/compressed_with_overviews.tif",
+                dtype_is_float=False,
             ),
         ],
     )
