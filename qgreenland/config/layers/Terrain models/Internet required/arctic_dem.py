afdf1264956fcb4d5db385f99cbfc04ce64184f6
Trey Stafford <trey.stafford@colorado.edu>
2023-08-31 14:59:31 -0600

    Add note about data addon download option for ArcticDEM

--- qgreenland/config/layers/Terrain models/Internet required/arctic_dem.py
+++ qgreenland/config/layers/Terrain models/Internet required/arctic_dem.py
@@ -4,7 +4,15 @@ from qgreenland.models.config.layer import Layer, LayerInput
 arctic_dem_online = Layer(
     id="arctic_dem",
     title="Arctic DEM",
-    description="Surface elevation in meters.",
+    description=(
+        """Surface elevation in meters.
+
+Note that the Polar Geospatial Center also provides custom download options for
+a 32m ArcticDEM for QGreenland users. Learn about these add-on data for
+QGreenland here:
+https://qgreenland.readthedocs.io/en/latest/user/reference/addons/arctic-dem-addon-data.html
+"""
+    ),
     in_package=True,
     tags=[],
     input=LayerInput(
