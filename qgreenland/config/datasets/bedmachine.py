4a84866f1b3d8febb4db1ab3319f822f2cee208c
Rikki Held <rikkiheld@gmail.com>
2026-02-06 16:56:05 -0700

    Change to citation

--- qgreenland/config/datasets/bedmachine.py
+++ qgreenland/config/datasets/bedmachine.py
@@ -17,19 +17,14 @@ This data set contains a bed topography/bathymetry map of Greenland based on
 mass conservation, multi-beam data, and other techniques. It also includes
 surface elevation and ice thickness data, as well as an ice/ocean/land mask.
 
-As a condition of using these data, we request that you acknowledge the
-author(s) of this data set by referencing the following peer-reviewed
-publication.
+As a condition of using these data, you must cite the use of this data set. 
+Such a practice gives credit to data set producers and advances principles of 
+transparency and reproducibility.
 
-Morlighem, M., C. Williams, E. Rignot, L. An, J. E. Arndt, J. Bamber,
-G. Catania, N. Chauché, J. A. Dowdeswell, B. Dorschel, I. Fenty, K. Hogan,
-I. Howat, A. Hubbard, M. Jakobsson, T. M. Jordan, K. K. Kjeldsen, R. Millan,
-L. Mayer, J. Mouginot, B. Noël, C. O'Cofaigh, S. J. Palmer, S. Rysgaard,
-H. Seroussi, M. J. Siegert, P. Slabon, F. Straneo, M. R. van den Broeke,
-W. Weinrebe, M. Wood, and K. Zinglersen. 2017. BedMachine v3: Complete bed
-topography and ocean bathymetry mapping of Greenland from multi-beam echo
-sounding combined with mass conservation, Geophysical Research
-Letters. 44. . https://doi.org/10.1002/2017GL074954 .
+Morlighem, M. et al. (2025). IceBridge BedMachine Greenland. (IDBMG4, Version 6). 
+[Data Set]. Boulder, Colorado USA. NASA National Snow and Ice Data Center 
+Distributed Active Archive Center. https://doi.org/10.5067/6B6B225B8V2D. 
+[describe subset used if applicable]. Date Accessed 02-06-2026.
 """,
         "citation": {
             "text": """
