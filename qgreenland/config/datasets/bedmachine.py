28ddc8a4c2a8476385d16ead83ba5213bf122579
Trey Stafford <trey.stafford@colorado.edu>
2026-02-10 16:13:59 -0700

    Cleanup redundant citation info and use `{{date_accessed}}` slug
    This slug gets filled in at system access time

--- qgreenland/config/datasets/bedmachine.py
+++ qgreenland/config/datasets/bedmachine.py
@@ -20,18 +20,13 @@ surface elevation and ice thickness data, as well as an ice/ocean/land mask.
 As a condition of using these data, you must cite the use of this data set.
 Such a practice gives credit to data set producers and advances principles of
 transparency and reproducibility.
-
-Morlighem, M. et al. (2025). IceBridge BedMachine Greenland. (IDBMG4, Version 6).
-[Data Set]. Boulder, Colorado USA. NASA National Snow and Ice Data Center
-Distributed Active Archive Center. https://doi.org/10.5067/6B6B225B8V2D.
-[describe subset used if applicable]. Date Accessed 02-06-2026.
 """,
         "citation": {
             "text": """
 Morlighem, M. et al. (2025). IceBridge BedMachine Greenland. (IDBMG4, Version 6).
 [Data Set]. Boulder, Colorado USA. NASA National Snow and Ice Data Center
 Distributed Active Archive Center. https://doi.org/10.5067/6B6B225B8V2D.
-[describe subset used if applicable]. Date Accessed 02-06-2026.
+[describe subset used if applicable]. {{date_accessed}}.
 """,
             "url": "https://doi.org/10.5067/6B6B225B8V2D",
         },
