bd9ca70527b11b9c3df1a93419a4c2563f7402ac
Trey Stafford <trey.stafford@colorado.edu>
2026-02-26 16:17:36 -0700

    Update municipalities layer with latest population data
    First multi-input layer!

---
+++ qgreenland/config/datasets/statbank.py
@@ -0,0 +1,36 @@
+from qgreenland.models.config.asset import HttpAsset
+from qgreenland.models.config.dataset import Dataset
+
+statbank = Dataset(
+    id="statbank",
+    assets=[
+        HttpAsset(
+            id="municipalities_2025_population",
+            # This URL returns a csv file `BEXSTB.csv` that has data for Jan 1, 2026
+            urls=["https://bank.stat.gl:443/sq/46cd875a-2a57-430b-ade9-e0baf23ecfe0"],
+        ),
+    ],
+    metadata={
+        "title": "Statistics Greenland",
+        # TODO: have team review abstract
+        "abstract": (
+            """You'll find all tables published by Statistics Greenland in our
+            StatBank. The tables are updated in parallel with publications on
+            our website. We aim to publish all tables in Greenlandic, Danish and
+            English.
+
+            Our StatBank is controlled by the program PX-Web, which for 25 years
+            has been developed by the Swedish National Statistics Office (
+            www.SCB.se ) in collaboration with many national statistical
+            offices. This means that the methods, you as a user of our StatBank
+            acquires, can be used to find statistics about many other countries,
+            including Sweden, Finland, Norway, Iceland, the Faroe Islands,
+            Aaland, Denmark,."""
+        ),
+        "citation": {
+            # TODO: have team review citation
+            "text": ("""Statistics Greenland."""),
+            "url": "https://stat.gl/default.asp?lang=en",
+        },
+    },
+)
--- qgreenland/config/layers/Reference/Borders/political_boundaries.py
+++ qgreenland/config/layers/Reference/Borders/political_boundaries.py
@@ -1,7 +1,9 @@
-import qgreenland.config.datasets.political_boundaries as dataset
+import qgreenland.config.datasets.political_boundaries as political_boundaries
+import qgreenland.config.datasets.statbank as statbank
 from qgreenland.config.helpers.steps.compressed_vector import compressed_vector
 from qgreenland.config.helpers.steps.ogr2ogr import ogr2ogr
 from qgreenland.models.config.layer import Layer, LayerInput
+from qgreenland.models.config.step import CommandStep
 
 nunagis_municipalities_population = Layer(
     id="nunagis_municipalities_population",
@@ -13,14 +15,61 @@ nunagis_municipalities_population = Layer(
     tags=[],
     style="nunagis_municipalities_population",
     inputs=[
+        # This input provides a multipolygon of municipalities and population numbers for 2019
         LayerInput(
-            dataset=dataset.nunagis_pop2019_municipalities,
-            asset=dataset.nunagis_pop2019_municipalities.assets["only"],
-        )
+            dataset=political_boundaries.nunagis_pop2019_municipalities,
+            asset=political_boundaries.nunagis_pop2019_municipalities.assets["only"],
+        ),
+        # This input provides updated population statistics for 2025 (Jan 1, 2026).
+        LayerInput(
+            dataset=statbank.statbank,
+            asset=statbank.statbank.assets["municipalities_2025_population"],
+        ),
     ],
     steps=[
+        CommandStep(
+            id="merge_datasets",
+            args=[
+                "ogr2ogr",
+                "{output_dir}/merged.gpkg",
+                "{input_dir}/fetched.geojson",
+                "-nln",
+                "municipalities",
+                "&&",
+                "ogr2ogr",
+                "-update",
+                "{output_dir}/merged.gpkg",
+                "{input_dir}/BEXSTB.csv",
+                "-nln",
+                "pop2025",
+            ],
+        ),
+        CommandStep(
+            id="join_data",
+            args=[
+                "ogr2ogr",
+                "{output_dir}/joined.gpkg",
+                "{input_dir}/merged.gpkg",
+                "-dialect",
+                "sqlite",
+                "-sql",
+                """\"SELECT
+                    municipalities.geom,
+                    municipalities.pop_municipality_2019_municip as municipality,
+                    municipalities.pop_municipality_2019_populatio as \\"Population 2019\\",
+                    pop2025.\\"Population January 1st\\" as \\"Population 2025\\"
+                    FROM municipalities
+                    JOIN pop2025 ON pop2025.municipality
+                    LIKE '%' || municipalities.pop_municipality_2019_municip || '%'\"""",
+                "-nln",
+                "municipalities_and_pop",
+            ],
+        ),
+        # Standard options
+        # TODO: necessary? Could merge standard options with ogr2ogr command
+        # above...
         *ogr2ogr(
-            input_file="{input_dir}/fetched.geojson",
+            input_file="{input_dir}/joined.gpkg",
             output_file="{output_dir}/nunagis_municipalities_population.gpkg",
         ),
     ],
@@ -37,8 +86,8 @@ ne_states_provinces = Layer(
     style="administrative_divisions",
     inputs=[
         LayerInput(
-            dataset=dataset.ne_states_provinces,
-            asset=dataset.ne_states_provinces.assets["only"],
+            dataset=political_boundaries.ne_states_provinces,
+            asset=political_boundaries.ne_states_provinces.assets["only"],
         )
     ],
     steps=[
@@ -57,8 +106,8 @@ ne_countries = Layer(
     style="countries",
     inputs=[
         LayerInput(
-            dataset=dataset.ne_countries,
-            asset=dataset.ne_countries.assets["only"],
+            dataset=political_boundaries.ne_countries,
+            asset=political_boundaries.ne_countries.assets["only"],
         )
     ],
     steps=[
